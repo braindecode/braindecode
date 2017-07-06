2fbf93d9ec3cb5b1a1318e7a36b1d7c008a3f182
Robin Tibor Schirrmeister <robintibor@gmail.com>
2017-07-06 18:55:02 +0200

    code for reproducing results on bcic iv 2a

---
+++ examples/bcic_iv_2a.py
@@ -0,0 +1,127 @@
+import logging
+import os.path
+import time
+from collections import OrderedDict
+import sys
+
+import numpy as np
+import torch.nn.functional as F
+from torch import optim
+
+from braindecode.models.deep4 import Deep4Net
+from braindecode.datasets.bcic_iv_2a import BCICompetition4Set2A
+from braindecode.experiments.experiment import Experiment
+from braindecode.experiments.monitors import LossMonitor, MisclassMonitor, \
+    RuntimeMonitor
+from braindecode.experiments.stopcriteria import MaxEpochs, NoDecrease, Or
+from braindecode.datautil.iterators import BalancedBatchSizeIterator
+from braindecode.models.shallow_fbcsp import ShallowFBCSPNet
+from braindecode.datautil.splitters import split_into_two_sets
+from braindecode.torch_ext.constraints import MaxNormDefaultConstraint
+from braindecode.torch_ext.util import set_random_seeds, np_to_var
+from braindecode.mne_ext.signalproc import mne_apply
+from braindecode.datautil.signalproc import (bandpass_cnt,
+                                             exponential_running_standardize)
+from braindecode.datautil.trial_segment import create_signal_target_from_raw_mne
+
+log = logging.getLogger(__name__)
+
+
+def run_exp(data_folder, subject_id, low_cut_hz, model, cuda):
+    train_filename = 'A{:02d}T.gdf'.format(subject_id)
+    test_filename = 'A{:02d}E.gdf'.format(subject_id)
+    train_filepath = os.path.join(data_folder, train_filename)
+    test_filepath = os.path.join(data_folder, test_filename)
+    train_label_filepath = train_filepath.replace('.gdf', '.mat')
+    test_label_filepath = test_filepath.replace('.gdf', '.mat')
+
+    train_loader = BCICompetition4Set2A(
+        train_filepath, labels_filename=train_label_filepath)
+    test_loader = BCICompetition4Set2A(
+        test_filepath, labels_filename=test_label_filepath)
+    train_cnt = train_loader.load()
+    test_cnt = test_loader.load()
+
+    # Preprocessing
+
+    train_cnt = train_cnt.drop_channels(['STI 014'])
+    # lets convert to millvolt for numerical stability of next operations
+    train_cnt = mne_apply(lambda a: a * 1e6, train_cnt)
+    train_cnt = mne_apply(
+        lambda a: bandpass_cnt(a, low_cut_hz, 38, train_cnt.info['sfreq'],
+                               filt_order=3,
+                               axis=1), train_cnt)
+    train_cnt = mne_apply(
+        lambda a: exponential_running_standardize(a.T, factor_new=1e-3,
+                                                  init_block_size=1000,
+                                                  eps=1e-4).T,
+        train_cnt)
+    test_cnt = test_cnt.drop_channels(['STI 014'])
+    test_cnt = mne_apply(lambda a: a * 1e6, test_cnt)
+    test_cnt = mne_apply(
+        lambda a: bandpass_cnt(a, low_cut_hz, 38, test_cnt.info['sfreq'],
+                               filt_order=3,
+                               axis=1), test_cnt)
+    test_cnt = mne_apply(
+        lambda a: exponential_running_standardize(a.T, factor_new=1e-3,
+                                                  init_block_size=1000,
+                                                  eps=1e-4).T,
+        test_cnt)
+
+    marker_def = OrderedDict([('Left Hand', [1]), ('Right Hand', [2],),
+                              ('Foot', [3]), ('Tongue', [4])])
+    ival = [-500, 4000]
+
+    train_set = create_signal_target_from_raw_mne(train_cnt, marker_def, ival)
+    test_set = create_signal_target_from_raw_mne(test_cnt, marker_def, ival)
+
+    train_set, valid_set = split_into_two_sets(train_set,
+                                               first_set_fraction=0.8)
+
+    set_random_seeds(seed=20190706, cuda=True)
+
+    n_classes = 4
+    n_chans = int(train_set.X.shape[1])
+    input_time_length = train_set.X.shape[2]
+    if model == 'shallow':
+        model = ShallowFBCSPNet(n_chans, n_classes, input_time_length=input_time_length,
+                            final_conv_length='auto').create_network()
+    elif model == 'deep':
+        model = Deep4Net(n_chans, n_classes, input_time_length=input_time_length,
+                            final_conv_length='auto').create_network()
+    if cuda:
+        model.cuda()
+
+    optimizer = optim.Adam(model.parameters())
+
+    iterator = BalancedBatchSizeIterator(batch_size=60)
+
+    stop_criterion = Or([MaxEpochs(1600),
+                         NoDecrease('valid_misclass', 160)])
+
+    monitors = [LossMonitor(), MisclassMonitor(), RuntimeMonitor()]
+
+    model_constraint = MaxNormDefaultConstraint()
+
+    exp = Experiment(model, train_set, valid_set, test_set, iterator=iterator,
+                     loss_function=F.nll_loss, optimizer=optimizer,
+                     model_constraint=model_constraint,
+                     monitors=monitors,
+                     stop_criterion=stop_criterion,
+                     remember_best_column='valid_misclass',
+                     run_after_early_stop=True, cuda=cuda)
+    exp.run()
+    return exp
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s %(levelname)s : %(message)s',
+                        level=logging.DEBUG, stream=sys.stdout)
+    # Should contain both .gdf files and .mat-labelfiles from competition
+    data_folder = '/home/schirrmr/data/bci-competition-iv/2a-gdf/'
+    subject_id = 1 # 1-9
+    low_cut_hz = 4 # 0 or 4
+    model = 'shallow' #'shallow' or 'deep'
+    cuda = True
+    exp = run_exp(data_folder, subject_id, low_cut_hz, model, cuda)
+    log.info("Last 10 epochs")
+    log.info("\n" + str(exp.epochs_df.iloc[-10:]))
---
+++ examples/bcic_iv_2a_cropped.py
@@ -0,0 +1,150 @@
+import logging
+import os.path
+import time
+from collections import OrderedDict
+import sys
+
+import numpy as np
+import torch.nn.functional as F
+from torch import optim
+import torch as th
+
+from braindecode.models.deep4 import Deep4Net
+from braindecode.models.util import to_dense_prediction_model
+from hyperoptim.parse import cartesian_dict_of_lists_product, \
+    product_of_list_of_lists_of_dicts
+from hyperoptim.util import save_pkl_artifact
+
+from braindecode.datasets.bcic_iv_2a import BCICompetition4Set2A
+from braindecode.experiments.experiment import Experiment
+from braindecode.experiments.monitors import LossMonitor, MisclassMonitor, \
+    RuntimeMonitor, CroppedTrialMisclassMonitor
+from braindecode.experiments.stopcriteria import MaxEpochs, NoDecrease, Or
+from braindecode.datautil.iterators import CropsFromTrialsIterator
+from braindecode.models.shallow_fbcsp import ShallowFBCSPNet
+from braindecode.datautil.splitters import split_into_two_sets
+from braindecode.torch_ext.constraints import MaxNormDefaultConstraint
+from braindecode.torch_ext.util import set_random_seeds, np_to_var
+from braindecode.mne_ext.signalproc import mne_apply
+from braindecode.datautil.signalproc import (bandpass_cnt,
+                                             exponential_running_standardize)
+from braindecode.datautil.trial_segment import create_signal_target_from_raw_mne
+
+log = logging.getLogger(__name__)
+
+
+def run_exp(data_folder, subject_id, low_cut_hz, model, cuda):
+    train_filename = 'A{:02d}T.gdf'.format(subject_id)
+    test_filename = 'A{:02d}E.gdf'.format(subject_id)
+    train_filepath = os.path.join(data_folder, train_filename)
+    test_filepath = os.path.join(data_folder, test_filename)
+    train_label_filepath = train_filepath.replace('.gdf', '.mat')
+    test_label_filepath = test_filepath.replace('.gdf', '.mat')
+
+    train_loader = BCICompetition4Set2A(
+        train_filepath, labels_filename=train_label_filepath)
+    test_loader = BCICompetition4Set2A(
+        test_filepath, labels_filename=test_label_filepath)
+    train_cnt = train_loader.load()
+    test_cnt = test_loader.load()
+
+    # Preprocessing
+
+    train_cnt = train_cnt.drop_channels(['STI 014'])
+    # lets convert to millvolt for numerical stability of next operations
+    train_cnt = mne_apply(lambda a: a * 1e6, train_cnt)
+    train_cnt = mne_apply(
+        lambda a: bandpass_cnt(a, low_cut_hz, 38, train_cnt.info['sfreq'],
+                               filt_order=3,
+                               axis=1), train_cnt)
+    train_cnt = mne_apply(
+        lambda a: exponential_running_standardize(a.T, factor_new=1e-3,
+                                                  init_block_size=1000,
+                                                  eps=1e-4).T,
+        train_cnt)
+    test_cnt.load_data()
+    test_cnt = test_cnt.drop_channels(['STI 014'])
+    test_cnt = mne_apply(lambda a: a * 1e6, test_cnt)
+    test_cnt = mne_apply(
+        lambda a: bandpass_cnt(a, low_cut_hz, 38, test_cnt.info['sfreq'],
+                               filt_order=3,
+                               axis=1), test_cnt)
+    test_cnt = mne_apply(
+        lambda a: exponential_running_standardize(a.T, factor_new=1e-3,
+                                                  init_block_size=1000,
+                                                  eps=1e-4).T,
+        test_cnt)
+
+    marker_def = OrderedDict([('Left Hand', [1]), ('Right Hand', [2],),
+                              ('Foot', [3]), ('Tongue', [4])])
+    ival = [-500, 4000]
+
+    train_set = create_signal_target_from_raw_mne(train_cnt, marker_def, ival)
+    test_set = create_signal_target_from_raw_mne(test_cnt, marker_def, ival)
+
+    train_set, valid_set = split_into_two_sets(train_set,
+                                               first_set_fraction=0.8)
+
+    set_random_seeds(seed=20190706, cuda=True)
+
+    n_classes = 4
+    n_chans = int(train_set.X.shape[1])
+    input_time_length=1000
+    if model == 'shallow':
+        model = ShallowFBCSPNet(n_chans, n_classes, input_time_length=input_time_length,
+                            final_conv_length=30).create_network()
+    elif model == 'deep':
+        model = Deep4Net(n_chans, n_classes, input_time_length=input_time_length,
+                            final_conv_length=2).create_network()
+
+    to_dense_prediction_model(model)
+    if cuda:
+        model.cuda()
+
+    dummy_input = np_to_var(train_set.X[:1, :, :, None])
+    if cuda:
+        dummy_input = dummy_input.cuda()
+    out = model(dummy_input)
+
+    n_preds_per_input = out.cpu().data.numpy().shape[2]
+
+    optimizer = optim.Adam(model.parameters())
+
+    iterator = CropsFromTrialsIterator(batch_size=60,
+                                       input_time_length=input_time_length,
+                                       n_preds_per_input=n_preds_per_input)
+
+    stop_criterion = Or([MaxEpochs(800),
+                         NoDecrease('valid_misclass', 80)])
+
+    monitors = [LossMonitor(), MisclassMonitor(col_suffix='sample_misclass'),
+                CroppedTrialMisclassMonitor(
+                    input_time_length=input_time_length), RuntimeMonitor()]
+
+    model_constraint = MaxNormDefaultConstraint()
+
+    loss_function = lambda preds, targets: F.nll_loss(
+        th.mean(preds, dim=2)[:, :, 0], targets)
+
+    exp = Experiment(model, train_set, valid_set, test_set, iterator=iterator,
+                     loss_function=loss_function, optimizer=optimizer,
+                     model_constraint=model_constraint,
+                     monitors=monitors,
+                     stop_criterion=stop_criterion,
+                     remember_best_column='valid_misclass',
+                     run_after_early_stop=True, cuda=cuda)
+    exp.run()
+    return exp
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s %(levelname)s : %(message)s',
+                        level=logging.DEBUG, stream=sys.stdout)
+    # Should contain both .gdf files and .mat-labelfiles from competition
+    data_folder = '/home/schirrmr/data/bci-competition-iv/2a-gdf/'
+    subject_id = 1 # 1-9
+    low_cut_hz = 4 # 0 or 4
+    model = 'shallow' #'shallow' or 'deep'
+    cuda = True
+    exp = run_exp(data_folder, subject_id, low_cut_hz, model, cuda)
+    log.info("Last 10 epochs")
+    log.info("\n" + str(exp.epochs_df.iloc[-10:]))
