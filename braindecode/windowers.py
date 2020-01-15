e7a56325a115d58dc8cae36863f1cbb492f2159f
Simon Brandt <simonbrandt@protonmail.com>
2020-01-15 18:50:24 +0100

    added event windower and unit test templates

--- braindecode/windowers.py
+++ braindecode/windowers.py
@@ -5,7 +5,8 @@ import numpy as np
 from sklearn.base import TransformerMixin
 import mne
 
-class Windower(TransformerMixin):
+
+class BaseWindower(TransformerMixin):
     """Fixed onset windower
     ToDo: samples or seconds
 
@@ -30,9 +31,29 @@ class Windower(TransformerMixin):
         return self
 
     def transform(self, X):
+        return NotImplementedError
+
+
+class FixedLengthWindower(BaseWindower):
+    """Fixed onset windower
+    ToDo: samples or seconds
+
+    Parameters
+    ----------
+    window_size_samples : int | None
+        size of one window in samples
+    overlap_size_samples : int
+        size of overlap for window in samples
+    drop_last_samples : bool, default=True
+        whether to drop the last samples when they don't fit into window
+
+    """
+
+    def __init__(self, window_size_samples, overlap_size_samples, drop_last_samples=True, tmin=0):
+        super().__init__(window_size_samples, overlap_size_samples, drop_last_samples, tmin)
+
+    def transform(self, X):
         """[summary]
-        ToDo: understand why in case windower = Windower(window_size_samples=1,
-                                                         overlap_size_samples=0) last event is dropped
         ToDo: id=1???
 
         Parameters
@@ -48,13 +69,58 @@ class Windower(TransformerMixin):
             duration = X.times[-1]
         else:
             duration = self.window_size_samples / fs
+
         overlap = self.overlap_size_samples / fs
-        events = mne.make_fixed_length_events(X, id=id_holder, duration=duration, overlap=overlap, stop=X.times[-1])
+        events = mne.make_fixed_length_events(X, id=id_holder, duration=duration, overlap=overlap, stop=None)
 
         if not self.drop_last_samples:
             last_valid_window_start = X.n_times - self.window_size_samples
             if events[-1, 0] < last_valid_window_start:
                 events = np.concatenate((events, [[last_valid_window_start, 0, id_holder]]))
+
+        windows = mne.Epochs(X, events, tmin=self.tmin, tmax=(self.window_size_samples - 1) / fs,
+                             baseline=None, preload=False)
+
+        return windows
+
+
+class EventWindower(BaseWindower):
+    """Fixed onset windower
+    ToDo: samples or seconds
+
+    Parameters
+    ----------
+    window_size_samples : int | None
+        size of one window in samples
+    overlap_size_samples : int
+        size of overlap for window in samples
+    drop_last_samples : bool, default=True
+        whether to drop the last samples when they don't fit into window
+
+    """
+
+    def __init__(self, window_size_samples, tmin=0, chunk_duration_samples=None,
+                 mapping=None):
+        super().__init__(window_size_samples, None, tmin=tmin)
+        self.chunk_duration_samples = chunk_duration_samples
+        self.mapping_rev = {v: k for k, v in mapping.items()}
+
+    def transform(self, X):
+        """[summary]
+        ToDo: id=1???
+        ToDo: plus epsilon 1e-6 on duration; otherwise perfect fitting chunk_durations will SOMETIMES(!!!) not fit
+
+        Parameters
+        ----------
+        X : mne.io.Raw
+            [description]
+        """
+
+        fs = X.info['sfreq']
+
+        X.annotations.duration += 1e-6  # see ToDo
+
+        events, _ = mne.events_from_annotations(X, self.mapping_rev, chunk_duration=self.chunk_duration_samples / fs)
 
         windows = mne.Epochs(X, events, tmin=self.tmin, tmax=(self.window_size_samples - 1) / fs,
                              baseline=None, preload=False)
