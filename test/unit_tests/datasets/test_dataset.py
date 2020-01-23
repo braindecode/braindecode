8cddda1807eee4fb9683386e0e730d43817e0018
Maciej Sliwowski <macieksliwowski@gmail.com>
2020-01-24 00:34:00 +0100

    tests using unittest changed to use pytest, pep8 fixes

--- test/unit_tests/datasets/test_dataset.py
+++ test/unit_tests/datasets/test_dataset.py
@@ -2,63 +2,65 @@
 #
 # License: BSD (3-clause)
 
-import unittest
-
 import mne
 import numpy as np
 import pandas as pd
+import pytest
 
 from braindecode.datasets import WindowsDataset
 
 
-class TestWindowsDataset(unittest.TestCase):
-    # TODO: add test for transformers and case when subject_info is used
-    def setUp(self):
-        # super(TestWindowsDataset, self).__init__()
-        rng = np.random.RandomState(42)
-        info = mne.create_info(ch_names=['0', '1'],
-                               sfreq=50, ch_types='eeg')
-        self.raw = mne.io.RawArray(data=rng.randn(2, 1000), info=info)
-        self.events = np.array([[100, 0, 1],
-                                [200, 0, 2],
-                                [300, 0, 1],
-                                [400, 0, 4],
-                                [500, 0, 3]])
-        self.supercrop_inds = [(0, 0, 100),
-                               (0, 100, 200),
-                               (1, 0, 100),
-                               (2, 0, 100),
-                               (2, 50, 150),]
-        metadata = pd.DataFrame(
-            {'sample': self.events[:,0],
-             'x': self.events[:,1],
-             'target': self.events[:,2],
-             'supercrop_inds': self.supercrop_inds})
-        self.mne_epochs = mne.Epochs(raw=self.raw, events=self.events,
-                                     metadata=metadata)
-        self.epochs_data = self.mne_epochs.get_data()
+# TODO: add test for transformers and case when subject_info is used
+@pytest.fixture(scope="module")
+def epochs_events_supercrop_dataset():
+    rng = np.random.RandomState(42)
+    info = mne.create_info(ch_names=['0', '1'],
+                           sfreq=50, ch_types='eeg')
+    raw = mne.io.RawArray(data=rng.randn(2, 1000), info=info)
+    events = np.array([[100, 0, 1],
+                       [200, 0, 2],
+                       [300, 0, 1],
+                       [400, 0, 4],
+                       [500, 0, 3]])
+    supercrop_idxs = [(0, 0, 100),
+                      (0, 100, 200),
+                      (1, 0, 100),
+                      (2, 0, 100),
+                      (2, 50, 150)]
+    metadata = pd.DataFrame(
+        {'sample': events[:, 0],
+         'x': events[:, 1],
+         'target': events[:, 2],
+         'supercrop_inds': supercrop_idxs,
+         'subject_info': None})
+    mne_epochs = mne.Epochs(raw=raw, events=events, metadata=metadata)
 
-        self.windows_dataset = WindowsDataset(self.mne_epochs, target="target")
+    windows_dataset = WindowsDataset(mne_epochs, target="target")
+    return mne_epochs, events, supercrop_idxs, windows_dataset
 
-    def test_get_item(self):
-        for i in range(len(self.epochs_data)):
-            x, y, inds = self.windows_dataset[i]
-            np.testing.assert_allclose(self.epochs_data[i], x)
-            self.assertEqual(self.events[i, 2], y,
-                             msg=f'Y not equal for epoch {i}')
-            self.assertEqual(self.supercrop_inds[i], inds,
-                             msg=f'Supercrop inds not equal for epoch {i}')
 
-    def test_len(self):
-        self.assertEqual(len(self.epochs_data), len(self.windows_dataset))
+def test_get_item(epochs_events_supercrop_dataset):
+    mne_epochs, events, supercrop_idxs, windows_dataset = epochs_events_supercrop_dataset
+    epochs_data = mne_epochs.get_data()
+    for i in range(len(epochs_data)):
+        x, y, inds = windows_dataset[i]
+        np.testing.assert_allclose(epochs_data[i], x)
+        assert events[i, 2] == y, f'Y not equal for epoch {i}'
+        assert supercrop_idxs[i] == inds, f'Supercrop inds not equal for epoch {i}'
 
-    def test_target_subject_info_is_none(self):
-        with self.assertRaises(AssertionError):
-            WindowsDataset(self.mne_epochs, target='is_none')
 
-    def test_target_in_subject_info(self):
-        with self.assertRaises(AssertionError):
-            WindowsDataset(self.mne_epochs, target='does_not_exist')
+def test_len(epochs_events_supercrop_dataset):
+    mne_epochs, _, _, windows_dataset = epochs_events_supercrop_dataset
+    assert len(mne_epochs.get_data()) == len(windows_dataset)
 
-if __name__ == '__main__':
-    unittest.main()
+
+def test_target_subject_info_is_none(epochs_events_supercrop_dataset):
+    mne_epochs = epochs_events_supercrop_dataset[0]
+    with pytest.raises(AssertionError):
+        WindowsDataset(mne_epochs, target='is_none')
+
+
+def test_target_in_subject_info(epochs_events_supercrop_dataset):
+    mne_epochs = epochs_events_supercrop_dataset[0]
+    with pytest.raises(AssertionError):
+        WindowsDataset(mne_epochs, target='does_not_exist')
--- test/unit_tests/datautil/test_windowers.py
+++ test/unit_tests/datautil/test_windowers.py
@@ -4,14 +4,13 @@
 #
 # License: BSD-3
 
+import mne
 import numpy as np
 import pytest
-import unittest
-import mne
 
-from braindecode.datautil.windowers import Windower
 from braindecode.datasets.moabb_datasets import MOABBFetcher
 from braindecode.datautil import FixedLengthWindower
+from braindecode.datautil.windowers import Windower
 
 
 @pytest.fixture(scope="module")
@@ -25,12 +24,12 @@ def mapping_ds_targets():
 def test_one_supercrop_per_original_trial(mapping_ds_targets):
     mapping, ds, targets = mapping_ds_targets
     windower = Windower(trial_start_offset_samples=0,
-                            trial_stop_offset_samples=1000,
-                            supercrop_size_samples=1000,
-                            supercrop_stride_samples=1,
-                            mapping=mapping)
+                        trial_stop_offset_samples=1000,
+                        supercrop_size_samples=1000,
+                        supercrop_stride_samples=1,
+                        mapping=mapping)
     windows = windower(ds[0])
-    description = windows.events[:,-1]
+    description = windows.events[:, -1]
     assert len(description) == 48
     np.testing.assert_array_equal(np.array(targets), description)
 
@@ -38,12 +37,12 @@ def test_one_supercrop_per_original_trial(mapping_ds_targets):
 def test_stride_has_no_effect(mapping_ds_targets):
     mapping, ds, targets = mapping_ds_targets
     windower = Windower(trial_start_offset_samples=0,
-                            trial_stop_offset_samples=1000,
-                            supercrop_size_samples=1000,
-                            supercrop_stride_samples=1000,
-                            mapping=mapping)
+                        trial_stop_offset_samples=1000,
+                        supercrop_size_samples=1000,
+                        supercrop_stride_samples=1000,
+                        mapping=mapping)
     windows = windower(ds[0])
-    description = windows.events[:,-1]
+    description = windows.events[:, -1]
     assert len(description) == 48
     np.testing.assert_array_equal(np.array(targets), description)
 
@@ -51,12 +50,12 @@ def test_stride_has_no_effect(mapping_ds_targets):
 def test_trial_start_offset(mapping_ds_targets):
     mapping, ds, targets = mapping_ds_targets
     windower = Windower(trial_start_offset_samples=-250,
-                            trial_stop_offset_samples=250,
-                            supercrop_size_samples=250,
-                            supercrop_stride_samples=250,
-                            mapping=mapping)
+                        trial_stop_offset_samples=250,
+                        supercrop_size_samples=250,
+                        supercrop_stride_samples=250,
+                        mapping=mapping)
     windows = windower(ds[0])
-    description = windows.events[:,-1]
+    description = windows.events[:, -1]
     assert len(description) == 96
     np.testing.assert_array_equal(targets, description[0::2])
     np.testing.assert_array_equal(targets, description[1::2])
@@ -65,12 +64,12 @@ def test_trial_start_offset(mapping_ds_targets):
 def test_shifting_last_supercrop_back_in(mapping_ds_targets):
     mapping, ds, targets = mapping_ds_targets
     windower = Windower(trial_start_offset_samples=-250,
-                            trial_stop_offset_samples=250,
-                            supercrop_size_samples=250,
-                            supercrop_stride_samples=300,
-                            mapping=mapping)
+                        trial_stop_offset_samples=250,
+                        supercrop_size_samples=250,
+                        supercrop_stride_samples=300,
+                        mapping=mapping)
     windows = windower(ds[0])
-    description = windows.events[:,-1]
+    description = windows.events[:, -1]
     assert len(description) == 96
     np.testing.assert_array_equal(targets, description[0::2])
     np.testing.assert_array_equal(targets, description[1::2])
@@ -79,12 +78,12 @@ def test_shifting_last_supercrop_back_in(mapping_ds_targets):
 def test_dropping_last_incomplete_supercrop(mapping_ds_targets):
     mapping, ds, targets = mapping_ds_targets
     windower = Windower(trial_start_offset_samples=-250,
-                            trial_stop_offset_samples=250,
-                            supercrop_size_samples=250,
-                            supercrop_stride_samples=300,
-                            drop_samples=True, mapping=mapping)
+                        trial_stop_offset_samples=250,
+                        supercrop_size_samples=250,
+                        supercrop_stride_samples=300,
+                        drop_samples=True, mapping=mapping)
     windows = windower(ds[0])
-    description = windows.events[:,-1]
+    description = windows.events[:, -1]
     assert len(description) == 48
     np.testing.assert_array_equal(targets, description)
 
@@ -92,12 +91,12 @@ def test_dropping_last_incomplete_supercrop(mapping_ds_targets):
 def test_maximally_overlapping_supercrops(mapping_ds_targets):
     mapping, ds, targets = mapping_ds_targets
     windower = Windower(trial_start_offset_samples=-2,
-                            trial_stop_offset_samples=1000,
-                            supercrop_size_samples=1000,
-                            supercrop_stride_samples=1,
-                            mapping=mapping)
+                        trial_stop_offset_samples=1000,
+                        supercrop_size_samples=1000,
+                        supercrop_stride_samples=1,
+                        mapping=mapping)
     windows = windower(ds[0])
-    description = windows.events[:,-1]
+    description = windows.events[:, -1]
     assert len(description) == 48 * 3
     np.testing.assert_array_equal(targets, description[0::3])
     np.testing.assert_array_equal(targets, description[1::3])
@@ -107,12 +106,12 @@ def test_maximally_overlapping_supercrops(mapping_ds_targets):
 def test_single_sample_size_supercrops(mapping_ds_targets):
     mapping, ds, targets = mapping_ds_targets
     windower = Windower(trial_start_offset_samples=0,
-                            trial_stop_offset_samples=1000,
-                            supercrop_size_samples=1,
-                            supercrop_stride_samples=1,
-                            mapping=mapping)
+                        trial_stop_offset_samples=1000,
+                        supercrop_size_samples=1,
+                        supercrop_stride_samples=1,
+                        mapping=mapping)
     windows = windower(ds[0])
-    description = windows.events[:,-1]
+    description = windows.events[:, -1]
     assert len(description) == 48000
     np.testing.assert_array_equal(targets, description[::1000])
     np.testing.assert_array_equal(targets, description[999::1000])
@@ -121,60 +120,54 @@ def test_single_sample_size_supercrops(mapping_ds_targets):
 def test_overlapping_trial_offsets(mapping_ds_targets):
     mapping, ds, targets = mapping_ds_targets
     windower = Windower(trial_start_offset_samples=-2000,
-                            trial_stop_offset_samples=1000,
-                            supercrop_size_samples=1000,
-                            supercrop_stride_samples=1000,
-                            mapping=mapping)
+                        trial_stop_offset_samples=1000,
+                        supercrop_size_samples=1000,
+                        supercrop_stride_samples=1000,
+                        mapping=mapping)
     with pytest.raises(AssertionError, match='trials overlap not implemented'):
         windower(ds[0])
 
 
-class TestFixedLengthWindower(unittest.TestCase):
-    # TODO: add tests for case with drop_last_sample==False
-    def setUp(self):
-        rng = np.random.RandomState(42)
-        self.sfreq = 50
-        info = mne.create_info(ch_names=['0', '1'],
-                               sfreq=self.sfreq, ch_types='eeg')
-        self.data = rng.randn(2, 1000)
-        self.raw = mne.io.RawArray(data=self.data, info=info)
+# TODO: add tests for case with drop_last_sample==False
+def test_fixed_length_windower():
+    rng = np.random.RandomState(42)
+    info = mne.create_info(ch_names=['0', '1'], sfreq=50, ch_types='eeg')
+    data = rng.randn(2, 1000)
+    raw = mne.io.RawArray(data=data, info=info)
 
-        # test case:
-        # (window_size_samples, overlap_size_samples, drop_last_samples,
-        # trial_start_offset_samples, n_windows)
-        self.test_cases = [
-            (100, 10, True, 0., 11),
-            # TODO: does using trial_start_offset_samples have sense?
-            # (100, 10, True, -0.5, 11),
-            (100, 50, True, 0., 19),
-            (None, 50, True, 0., 1)
-        ]
+    # test case:
+    # (window_size_samples, overlap_size_samples, drop_last_samples,
+    # trial_start_offset_samples, n_windows)
+    test_cases = [
+        (100, 10, True, 0., 11),
+        # TODO: does using trial_start_offset_samples have sense?
+        # (100, 10, True, -0.5, 11),
+        (100, 50, True, 0., 19),
+        (None, 50, True, 0., 1)
+    ]
 
-    def test_windower_results(self):
-        for test_case in self.test_cases:
-            (window_size, overlap_size, drop_last_samples,
-             trial_start_offset_samples, n_windows) = test_case
-            windower = FixedLengthWindower(
-                window_size_samples=window_size,
-                overlap_size_samples=overlap_size,
-                drop_last_samples=drop_last_samples,
-                trial_start_offset_samples=trial_start_offset_samples)
+    for i, test_case in enumerate(test_cases):
+        (window_size, overlap_size, drop_last_samples,
+         trial_start_offset_samples, n_windows) = test_case
+        windower = FixedLengthWindower(
+            window_size_samples=window_size,
+            overlap_size_samples=overlap_size,
+            drop_last_samples=drop_last_samples,
+            trial_start_offset_samples=trial_start_offset_samples)
 
-            epochs = windower(self.raw)
-            epochs_data = epochs.get_data()
-            if window_size is None:
-                window_size = self.data.shape[1]
-            idxs = np.arange(0,
-                             self.data.shape[1] - window_size + 1,
-                             window_size - overlap_size)
+        epochs = windower(raw)
+        epochs_data = epochs.get_data()
+        if window_size is None:
+            window_size = data.shape[1]
+        idxs = np.arange(
+            0, data.shape[1] - window_size + 1, window_size - overlap_size)
 
-            self.assertEqual(len(idxs), epochs_data.shape[0])
-            self.assertEqual(window_size, epochs_data.shape[2])
-            for i, idx in enumerate(idxs):
-                np.testing.assert_allclose(
-                    self.data[:, idx: idx + window_size], epochs_data[i, :]
-                )
-
-
-if __name__ == '__main__':
-    unittest.main()
+        assert len(idxs) == epochs_data.shape[0], \
+            f"Number of epochs different than expected for test case {i}"
+        assert window_size == epochs_data.shape[2], \
+            f"Window size different than expected for test case {i}"
+        for j, idx in enumerate(idxs):
+            np.testing.assert_allclose(
+                data[:, idx: idx + window_size], epochs_data[j, :],
+                err_msg=f"Epochs different for test case {i} for epoch {j}"
+            )
