a971493fd51167a693b0eb9cf9b4b7fdfde29007
Hubert Banville <hubert.jbanville@gmail.com>
2020-02-07 15:18:45 +0100

    standardize python version to 3.7

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 
     # What does your project relate to?
