c4ca029eb7e46d589a2f636a36eba2e24bc6d4f5
sliwy <macieksliwowski@gmail.com>
2020-05-06 00:35:38 +0200

    first additions to trialwise decoding tutorial

--- examples/plot_bcic_iv_2a_moabb_trial.py
+++ examples/plot_bcic_iv_2a_moabb_trial.py
@@ -1,6 +1,23 @@
 """
 Trialwise Decoding on BCIC IV 2a Dataset
 ========================================
+
+This tutorial shows you how to train and test deep learning models with Braindecode. Whole procedure is performed on
+MOABB BCI IV dataset. Braindeocde supplies infrastructure for loading, transforming and splitting all MOABB datasets
+which is also briefly presented here.
+
+This script presents a standard processing workflow using trialwise braindecode models. It is also compatible with
+Pytorch deep learning models created by user. There are no additional constraints on model's implementations except
+being valid Pytorch model and correct data processing (it does not have to inherit from any additional class or
+implement any additional method).
+
+Trialwise decoding is one out of two ways of EEG signal processing implemented in Braindecode. Main of trialwise
+decoding:
+
+1. A complete trial is pushed through the network
+2. The network produces a prediction
+3, The prediction is compared to the target (label) for that trial to compute the loss
+
 """
 
 # Authors: Maciej Sliwowski <maciek.sliwowski@gmail.com>
@@ -11,15 +28,14 @@ Trialwise Decoding on BCIC IV 2a Dataset
 # License: BSD-3
 from functools import partial
 
-import numpy as np
 import torch
 import mne
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 from skorch.callbacks import LRScheduler
 from skorch.helper import predefined_split
-mne.set_log_level('ERROR')
 
 from braindecode import EEGClassifier
 from braindecode.datautil import create_windows_from_events
@@ -29,29 +45,38 @@ from braindecode.models import ShallowFBCSPNet
 from braindecode.util import set_random_seeds
 from braindecode.datautil.signalproc import exponential_running_standardize
 from braindecode.datautil.transforms import transform_concat_ds
+mne.set_log_level('ERROR')
 
-subject_id = 3  # 1-9
-model_name = "shallow"  # 'shallow' or 'deep'
-low_cut_hz = 4.  # 0 or 4
-n_epochs = 5
+########################################################################################################################
+# Set parameters
+# ^^^^^^^^^^^^^^^^^
 seed = 20200220
 
+# Parameters describing the dataset and transformations
+subject_id = 3  # 1-9
+low_cut_hz = 4.  # 0 or 4
 high_cut_hz = 38.
+n_classes = 4  # number of classes to predict
+n_chans = 22  # number of channels in the dataset
 trial_start_offset_seconds = -0.5
-input_time_length = 1125
-batch_size = 64
+input_time_length = 1125  # length of trial in samples
 factor_new = 1e-3
 init_block_size = 1000
-cuda = torch.cuda.is_available()
+
+# Select model's name and define parameters describing training
+model_name = "shallow"  # 'shallow' or 'deep'
+n_epochs = 5  # number of epochs of training
+batch_size = 64
+cuda = torch.cuda.is_available()  # check if GPU is available, if True chooses to use it
 device = 'cuda' if cuda else 'cpu'
 if cuda:
     torch.backends.cudnn.benchmark = True
 
-n_classes = 4
-n_chans = 22
-
+# Set random seed to be able to reproduce results
 set_random_seeds(seed=seed, cuda=cuda)
 
+# Create model
+# ^^^^^^^^^^^^^^^^^
 if model_name == "shallow":
     model = ShallowFBCSPNet(
         n_chans,
@@ -72,8 +97,12 @@ elif model_name == "deep":
     lr = 1 * 0.01
     weight_decay = 0.5 * 0.001
 
+# Send model to GPU
 if cuda:
     model.cuda()
+
+# Load and transform dataset
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 dataset = MOABBDataset(dataset_name="BNCI2014001", subject_ids=[subject_id])
 
@@ -88,9 +117,9 @@ raw_transform_dict = [
 ]
 transform_concat_ds(dataset, raw_transform_dict)
 
-sfreqs = [ds.raw.info['sfreq'] for ds in dataset.datasets]
-assert len(np.unique(sfreqs)) == 1
-trial_start_offset_samples = int(trial_start_offset_seconds * sfreqs[0])
+# # sfreqs = [ds.raw.info['sfreq'] for ds in dataset.datasets]
+# # assert len(np.unique(sfreqs)) == 1
+# trial_start_offset_samples = int(trial_start_offset_seconds * sfreqs[0])
 
 windows_dataset = create_windows_from_events(
     dataset,
