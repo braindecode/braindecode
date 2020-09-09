ce3e1f388d2a0ce42b57cb20c514818ff15aeb27
Simon Freyburger <simonfreyburger@yahoo.fr>
2020-09-09 10:33:14 +0200

    added sleepphysionet

--- braindecode/datasets/__init__.py
+++ braindecode/datasets/__init__.py
@@ -4,3 +4,4 @@ Loader code for some datasets.
 from .base import WindowsDataset, BaseDataset, BaseConcatDataset
 from .moabb import MOABBDataset
 from .tuh import TUHAbnormal
+from .sleep_physionet import SleepPhysionet
