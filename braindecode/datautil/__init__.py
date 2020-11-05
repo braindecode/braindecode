d1f33fbc53a261f12344e2622882109daecf1149
Simon Freyburger <simonfreyburger@yahoo.fr>
2020-11-05 11:30:44 +0100

    fix imports

--- braindecode/datautil/__init__.py
+++ braindecode/datautil/__init__.py
@@ -4,7 +4,6 @@ Utilities for data manipulation.
 
 from .windowers import create_windows_from_events, create_fixed_length_windows
 from .preprocess import zscore, scale, exponential_moving_demean, exponential_moving_standardize, filterbank
-from .transform_side_funcs import mask_along_axis
 from .xy import create_from_X_y
 from .mne import create_from_mne_raw, create_from_mne_epochs
 from .serialization import save_concat_dataset, load_concat_dataset
