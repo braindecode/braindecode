f98d3bdd0121a43a7f0465a68fde2517649ca5a0
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2021-06-20 15:34:56 +0200

    more flake8 + mimic MNE config

--- braindecode/datautil/xy.py
+++ braindecode/datautil/xy.py
@@ -2,7 +2,8 @@
 #
 # License: BSD (3-clause)
 
-from ..datasets.xy import *  # noqa: F401,F403
 from warnings import warn
+from ..datasets.xy import *  # noqa: F401,F403
 
-warn('datautil.xy module is deprecated and is now under datasets.xy, please use from import braindecode.datasets.xy')
+warn('datautil.xy module is deprecated and is now under '
+     'datasets.xy, please use from import braindecode.datasets.xy')
