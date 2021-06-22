486dc66ea2f458d77cc7db5d257fb21cd8a29c37
“l-omar-chehab“ <l.omarchehab@gmail.com>
2021-06-22 15:59:16 +0200

    refactored some code

--- braindecode/models/usleep_wip.py
+++ braindecode/models/usleep_wip.py
@@ -13,6 +13,109 @@ from torch.nn.modules.batchnorm import BatchNorm1d
 
 # TODO: check extra params
 
+class EncoderBlock(nn.Module):
+    '''Encoding block for a timeseries x of shape (B, C, T).
+    With each new block (depth):
+        -- the temporal dimension shrinks (via maxpooling in the time-domain)
+        -- the spatial dimension expands (via more conv1d filters in the time-domain).
+    both do so at exponential rates.
+    '''
+    def __init__(self,
+                 depth=3,
+                 in_channels=2,
+                 out_channels=2,
+                 kernel_size=9,
+                 downsample=2,
+                 complexity_factor=2):
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size  # choose odd nb
+        self.complexity_factor = complexity_factor
+        self.downsample = downsample
+        padding = (kernel_size - 1) // 2   # chosen to preserve dimension
+        assert kernel_size % 2 == 1, 'Choose kernel_size to be an odd number.'
+
+        self.encoder_block = nn.Sequential(
+                nn.Conv1d(in_channels=in_channels, 
+                            out_channels=out_channels, 
+                            kernel_size=kernel_size, 
+                            padding=padding),
+                nn.ELU(),
+                nn.BatchNorm1d(num_features=out_channels),
+            )
+
+    def forward(self, x):
+        x = self.encoder_block(x)
+        residual = x
+        x = nn.MaxPool1d(kernel_size=self.downsample)(x)
+        return x, residual
+
+
+class DecoderBlock(nn.Module):
+    '''Decoding block for a timeseries x of shape (B, C, T).
+    With each new block (depth):
+        -- the temporal dimension expands (via upsampling in the time-domain)
+        -- the spatial dimension is preserved
+    the first does so at an exponential rate.
+    '''
+    def __init__(self,
+                 depth=3,
+                 in_channels=2,
+                 out_channels=2,
+                 kernel_size=9,
+                 upsample=2,
+                 complexity_factor=2):
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size  # choose odd nb
+        self.complexity_factor = complexity_factor
+        self.upsample = upsample
+        padding = (kernel_size - 1) // 2   # chosen to preserve dimension
+        assert kernel_size % 2 == 1, 'Choose kernel_size to be an odd number.'
+
+        self.decoder_block_preskip = nn.Sequential(
+                    nn.Upsample(scale_factor=upsample),
+                    nn.Conv1d(in_channels=in_channels, 
+                              out_channels=out_channels, 
+                              kernel_size=kernel_size, 
+                              padding=padding),
+                    nn.ELU(),
+                    nn.BatchNorm1d(num_features=out_channels),
+                )
+        self.decoder_block_postskip = nn.Sequential(
+                    nn.Conv1d(in_channels=2 * out_channels, 
+                              out_channels=out_channels, 
+                              kernel_size=kernel_size, 
+                              stride=1, 
+                              padding=padding),  # to preserve dimension (check)
+                    nn.ELU(),
+                    nn.BatchNorm1d(num_features=out_channels),
+                )
+
+    def forward(self, x, residual):
+        x = self.decoder_block_preskip(x)
+        x = torch.cat([x, residual], axis=1) # (B, 2 * C, T)
+        x = self.decoder_block_postskip(x)
+        return x
+
+
+
+# Small testing script
+batch_size, n_channels, n_times = 64, 2, 3000
+x = torch.Tensor(batch_size, n_channels, n_times)
+encoder = EncoderBlock(in_channels=2, out_channels=4)
+decoder = DecoderBlock(in_channels=4, out_channels=2)
+z, residual = encoder(x)
+x_hat = decoder(z, residual)
+
+
+
+
+
+
+
 class USleep(nn.Module):
 
     def __init__(self, 
