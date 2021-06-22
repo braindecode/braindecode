9f1ad92ed7ef2b1a0b7281811d612a118819abd4
“l-omar-chehab“ <l.omarchehab@gmail.com>
2021-06-22 12:19:44 +0200

    First iteration on USleep written in torch.

--- braindecode/models/usleep_wip.py
+++ braindecode/models/usleep_wip.py
@@ -7,104 +7,130 @@ import numpy as np
 import torch
 from torch import nn
 from torch.nn import init
-from torch.nn.functional import elu
+from torch.nn.functional import elu, upsample
+from torch.nn.modules.batchnorm import BatchNorm1d
 
+
+# TODO: check extra params
 
 class USleep(nn.Module):
 
     def __init__(self, 
-                n_classes=5,
-                batch_shape=128,
-                depth=12,
-                dilation=1,
-                activation="elu",
-                dense_classifier_activation="tanh",
-                kernel_size=9,
-                transition_window=1,
-                #  padding="same",
-                filters=5,
-                complexity_factor=2,
-                l2_reg=None,
-                data_per_prediction=None):
-        '''TODO: remove redundant arguments.'''
-        super().__init__()
-
-
-        # Step 1: write self.attribute = default
-
-
-        # Step 2: instantiate encoder
-        self.unet = # to be defined
-        self.clf = # 
-
-
-        for idx in range(depth):
-
-
-        # residual_connections = []
-        for i in range(depth):
-            conv = Conv2D(int(filters*complexity_factor), (kernel_size, 1),
-                          activation=activation, padding=padding,
-                          kernel_regularizer=regularizer,
-                          bias_regularizer=regularizer,
-                          dilation_rate=dilation,
-                          name=l_name + "_conv1")(in_)
-            bn = BatchNormalization(name=l_name + "_BN1")(conv)
-            s = bn.get_shape()[1]
-            if s % 2:
-                bn = ZeroPadding2D(padding=[[1, 0], [0, 0]],
-                                   name=l_name + "_padding")(bn)
-            in_ = MaxPooling2D(pool_size=(2, 1), name=l_name + "_pool")(bn)
-
-            # add bn layer to list for residual conn.
-            residual_connections.append(bn)
-            filters = int(filters * np.sqrt(2))
-
-
-
-
-        return 0
-
-
-
-
-    """
-    def __init__(self,
-                 n_classes,
-                 batch_shape,
+                 n_classes=5,
                  depth=12,
                  dilation=1,
-                 activation="elu",
                  dense_classifier_activation="tanh",
                  kernel_size=9,
                  transition_window=1,
-                 padding="same",
-                 init_filters=5,
-                 complexity_factor=2,
-                 l2_reg=None,
-                 data_per_prediction=None,
-                 logger=None,
-                 build=True,
-                 **kwargs):
+                 filters_init=5,
+                 complexity_factor=2):
+        '''TODO: remove redundant arguments.'''
+        super().__init__()
+
+        # Set attributes
+        padding = (kernel_size - 1) // 2   # to preserve dimension (check)
+    
+        # Instantiate encoder : input has shape (B, C, T)
+        encoder = []
+        filters = filters_init
+        for _ in range(depth):
+            # update nb of input / output channels
+            in_channels = 2 if _ == 0 else out_channels
+            out_channels = int(filters * complexity_factor)
+
+            # add encoder block (down)
+            encoder += [
+                nn.Sequential(
+                    nn.Conv1d(in_channels=in_channels, 
+                              out_channels=out_channels, 
+                              kernel_size=kernel_size, 
+                              stride=1, 
+                              padding=padding),
+                    nn.ELU(),
+                    nn.BatchNorm1d(num_features=out_channels),
+                )
+            ]
+            
+            # update nb of filters
+            filters = int(filters * np.sqrt(2))
+        self.encoder = nn.Sequential(*encoder)
+
+        # Instantiate bottom
+        in_channels = out_channels
+        out_channels = int(filters * complexity_factor)
+        bottom = nn.Sequential(
+                    nn.Conv1d(in_channels=in_channels, 
+                              out_channels=out_channels, 
+                              kernel_size=kernel_size, 
+                              stride=1, 
+                              padding=padding),
+                    nn.ELU(),
+                    nn.BatchNorm1d(num_features=out_channels),
+                )
+
+        # Instantiate decoder
+        decoder_preskip = []
+        decoder_postskip = []
+
+        for _ in range(depth):
+
+            # add decoder blocks (up)
+            decoder_preskip += [
+                nn.Sequential(
+                    nn.Upsample(scale_factor=2),
+                    nn.Conv1d(in_channels=in_channels, 
+                              out_channels=out_channels, 
+                              kernel_size=kernel_size, 
+                              stride=1, 
+                              padding=(kernel_size - 1) // 2),
+                    nn.ELU(),
+                    nn.BatchNorm1d(num_features=out_channels),
+                )
+            ]
+            
+            # we will concatenate channels via a skip connection, so they multiply by 2
+            in_channels *= 2
+
+            # add encoder block (down)
+            decoder_postskip += [
+                nn.Sequential(
+                    nn.Conv1d(in_channels=in_channels, 
+                              out_channels=out_channels, 
+                              kernel_size=kernel_size, 
+                              stride=1, 
+                              padding=(kernel_size - 1) // 2),  # to preserve dimension (check)
+                    nn.ELU(),
+                    nn.BatchNorm1d(num_features=out_channels),
+                )
+            ]
+
+        self.decoder_preskip = nn.Sequential(*decoder_preskip)
+        self.decoder_postskip = nn.Sequential(*decoder_postskip)
 
 
+    def forward(self, x):
+        '''Input x has shape (B, C, T).'''
+        
+        # encoder
+        residuals = []
+        for down in self.encoder:
+            x = down(x)
+            residuals.append(x)
+            x = nn.MaxPool1d(kernel_size=2)(x)
+
+        # decoder
+        residuals = residuals[::-1]  # in order of up layers
+        for (idx, (up_preskip, up_postskip)) in enumerate(zip(self.decoder_preskip, self.decoder_postskip)):
+            x = up_preskip(x)
+            x = torch.cat([x, residuals[idx]], axis=1) # (B, 2 * C, T)
+            x = up_postskip(x)
+        
+        return x
+
+    # self.clf = # 
 
 
-
-
-# Test model on a single input
-sfreq = 100  # unit: Hz
-time_window = 30  # unit: s
-batch_size, n_times, n_channels = 1024, time_window * sfreq, 2 
-
-x = torch.randn(batch_size, n_channels, n_times)
-usleep = USleep()
-y = usleep(x)
-print("x has shape: ", x.shape)
-print("y has shape: ", y.shape)
-
-
-
-
-
-
+# Small testing script
+batch_size, n_channels, n_times = 1024, 2, 3000
+x = torch.Tensor(batch_size, n_channels, n_times)
+model = USleep()
