b22793a013c6e0ef00eee86a970ac0c8d4e07ea7
Mohammad Fattouh <mo.fattouh@gmail.com>
2021-06-23 19:14:52 +0200

    import TimeSeriesLoss to training module

--- braindecode/training/__init__.py
+++ braindecode/training/__init__.py
@@ -3,6 +3,6 @@ Functionality for skorch-based training.
 """
 
 
-from .losses import CroppedLoss, mixup_criterion
+from .losses import CroppedLoss, mixup_criterion, TimeSeriesLoss
 from .scoring import (CroppedTrialEpochScoring, PostEpochTrainScoring,
                       trial_preds_from_window_preds, predict_trials)
