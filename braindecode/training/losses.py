f9a199fafc5f85374fbc5b7e314b042a6ad0b67b
Mohammad Fattouh <mo.fattouh@gmail.com>
2021-06-23 16:47:04 +0200

    add Timeseries target loss

--- braindecode/training/losses.py
+++ braindecode/training/losses.py
@@ -30,6 +30,39 @@ class CroppedLoss(nn.Module):
         return self.loss_function(avg_preds, targets)
 
 
+class TimeSeriesLoss(nn.Module):
+    """Compute Loss between timeseries targets and predictions.
+    Assumes predictions are in shape:
+    n_batch size x n_classes x n_predictions (in time)
+    Assumes targets are in shape:
+    n_batch size x n_classes x window_len (in time)
+    If the targets contain NaNs, the NaNs will be masked out and the loss will be only computed for
+    predictions valid corresponding to valid target values."""
+
+    def __init__(self, loss_function):
+        super().__init__()
+        self.loss_function = loss_function
+
+    def forward(self, preds, targets):
+        """Forward pass.
+
+        Parameters
+        ----------
+        preds: torch.Tensor
+            Model's prediction with shape (batch_size, n_classes, n_times).
+        targets: torch.Tensor
+            Target labels with shape (batch_size, n_classes, n_times).
+        """
+        num_preds = preds.shape[-1]
+        targets_mask = ~torch.isnan(targets)
+        # slice the targets mask to fit preds shape
+        preds_mask = targets_mask[:, :, -num_preds:]
+        # select valid targets that have a matching predictions
+        masked_targets = targets[targets_mask][:, :, -num_preds:]
+        masked_preds = preds[preds_mask]
+        return self.loss_function(masked_preds, masked_targets)
+
+
 def mixup_criterion(preds, target):
     """Implements loss for Mixup for EEG data. See [1]_.
 
