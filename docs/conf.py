c20100c5a4195adf40d3c2644f16116868fd888f
Erik Bjäreholt <erik@bjareho.lt>
2021-07-07 19:28:57 +0200

    docs: make all navigation entries expandable (#296)

--- docs/conf.py
+++ docs/conf.py
@@ -218,7 +218,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {"collapse_navigation": False, "navigation_depth": 4}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
