Fix rotation transform in case of learnable param
Within the function _torch_normalize_vector, which is used in
SensorsRotation transform, there is a bug that may happen when using
learnable nn.Parameter as the attribute (maximum angle): if the mask
made of elements with non-zero norm is made only of `True` elements,
then there is a chance that pytorch crashes for no reason saying that
the `number of dims don't match in permute` (RuntimeError). This bug is
very difficult to reproduce (and hence to understand precisely), but it
seems to be related to pytorch itself. Indeed, the function is super
simple and trying to reproduce the bug with the exact same vectors in
the exact same conditions won't consistently lead to the error (there is
no shape mismatch or anything of the sort).

while this is disturbing, it appears that simply not passing the mask to
the vector in the case of a useless mask made only of `True` values will
solve the problem, which is the purpose of this commit. A test for it is
added in a commit to follow.

braindecode/augmentation/functional.py
@@ -581,7 +581,13 @@ def _torch_normalize_vectors(rr):
     new_rr = rr.clone()
     size = torch.linalg.norm(rr, axis=1)
     mask = (size > 0)
-    new_rr[mask] = rr[mask] / size[mask].unsqueeze(-1)
+    if not torch.all(mask):
+        new_rr[mask] = rr[mask] / size[mask].unsqueeze(-1)
+    else:
+        # For some reason, this distinction is necessary when the input (rr)
+        # depends on learnable parameters. Passing a mask made of all True
+        # will lead to a RuntimeError: number of dims don't match in permute
+        new_rr = rr / size.unsqueeze(-1)
     return new_rr
 
 