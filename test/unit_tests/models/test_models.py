fcf4950ad5272e670a883486c20aa63c1abbc1c7
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2022-06-16 02:44:55 -0300

    Adding draft of the test. Fix later.

--- test/unit_tests/models/test_models.py
+++ test/unit_tests/models/test_models.py
@@ -12,7 +12,7 @@ import pytest
 
 from braindecode.models import (
     Deep4Net, EEGNetv4, EEGNetv1, HybridNet, ShallowFBCSPNet, EEGResNet, TCN,
-    SleepStagerChambon2018, SleepStagerBlanco2020, SleepStagerEldele2021, USleep, TIDNet)
+    SleepStagerChambon2018, SleepStagerBlanco2020, SleepStagerEldele2021, USleep, TIDNet, EEGInception)
 from braindecode.util import set_random_seeds
 
 
@@ -108,6 +108,50 @@ def test_tcn(input_sizes):
         n_filters=5, n_blocks=2, kernel_size=4, drop_prob=.5,
         add_log_softmax=True)
     check_forward_pass(model, input_sizes, only_check_until_dim=2)
+
+def test_eeginception(input_sizes):
+    model = EEGInception(
+        input_sizes['n_channels'], input_sizes['n_classes'],
+        input_size_s=input_sizes['n_in_times'])
+    check_forward_pass(model, input_sizes)
+
+def test_eeginception_n_params():
+    """Make sure the number of parameters is the same as in the paper when
+    using the same architecture hyperparameters.
+    """
+    model = EEGInception(
+        in_chans=2, sfreq=128, depth=12, n_time_filters=5,
+        complexity_factor=1.67, with_skip_connection=True, n_classes=5,
+        input_size_s=30, time_conv_size_s=9 / 128)
+
+    n_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    assert n_params == 3114337  # From paper's supplementary materials, Table 2
+
+@pytest.mark.parametrize('in_chans,sfreq,n_classes,input_size_s',
+                         [(20, 128, 5, 30), (10, 100, 4, 20), (1, 64, 2, 30)])
+def test_eeginception(in_chans, sfreq, n_classes, input_size_s):
+    rng = np.random.RandomState(42)
+    n_examples = 10
+    seq_length = 3
+
+    model = EEGInception(
+        in_chans=in_chans, sfreq=sfreq, n_classes=n_classes,
+        input_size_s=input_size_s, ensure_odd_conv_size=True)
+    model.eval()
+
+    X = rng.randn(n_examples, in_chans, int(sfreq * input_size_s))
+    X = torch.from_numpy(X.astype(np.float32))
+
+    y_pred1 = model(X)  # 3D inputs : (batch, channels, time)
+    y_pred2 = model(X.unsqueeze(1))  # 4D inputs : (batch, 1, channels, time)
+    y_pred3 = model(torch.stack([X for idx in range(seq_length)],
+                                axis=1))  # (batch, sequence, channels, time)
+    assert y_pred1.shape == (n_examples, n_classes)
+    assert y_pred2.shape == (n_examples, n_classes)
+    assert y_pred3.shape == (n_examples, n_classes, seq_length)
+    np.testing.assert_allclose(y_pred1.detach().cpu().numpy(),
+                               y_pred2.detach().cpu().numpy())
+
 
 
 @pytest.mark.parametrize('n_channels,sfreq,n_classes,input_size_s',
