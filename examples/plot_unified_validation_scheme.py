149c73adc2565f492a26f544d2ec0428a6425ca2
martinwimpff <ac132249@uni-stuttgart.de>
2022-08-05 09:30:18 +0200

    implemented further suggestions

--- examples/plot_unified_validation_scheme.py
+++ examples/plot_unified_validation_scheme.py
@@ -3,36 +3,45 @@ Unified Validation Scheme
 ========================================
 
 This tutorial shows you how to properly train, tune and test your deep learning
-models with Braindecode. We will use the BCIC IV 2a dataset as a showcase example,
-however this scheme holds for standard supervised trial-based decoding setting.
+models with Braindecode. We will use the BCIC IV 2a dataset as a showcase example.
+
+This scheme holds for standard supervised trial-based decoding setting.
 As this tutorial will include additional parts of code like loading and preprocessing,
-defining a model, etc. which are not exclusive to this page (compare `Trialwise Decoding
-Tutorial <./plot_bcic_iv_2a_moabb_trial.html>`__), feel free to skip these parts.
-In general we distinguish between 3 different validation schemes, one for the final training and two
-different methods for tuning/hyperparameter search.
+defining a model, and other details which are not exclusive to this page (compare
+`Cropped Decoding Tutorial <./plot_bcic_iv_2a_moabb_trial.html>`__).
+
+In general we distinguish between 3 different validation schemes, one for the final training
+and two different methods for tuning/hyperparameter search.
 
 """
 
 ######################################################################
 # Why should I care about model evaluation?
 # -------------------------------------
-# Short answer: To produce reliable results.
-# To train a Machine Learning model you typically use two distinct
-# datasets: one for training and one for testing. Easy - right?
-# But the story does not end here. While developing a ML model you
-# usually have to adjust and tune hyperparameters of your model/
+# Short answer: To produce reliable results!
+#
+# In machine learning, we usually follow the scheme of splitting the
+# data into two parts, training and testing sets. It sounds like a
+# simple division, right? But the story does not end here.
+#
+# While developing a ML model you
+# usually have to adjust and tune hyperparameters of your model or
 # pipeline (e.g., number of layers, learning rate, number of epochs).
-# If you would keep using the test dataset to evaluate your adjustment
-# you would run into something called data leakage. This means that,
-# by using the test set to adjust the hyperparameters of your model,
-# the model implicitly learns from the test set. Therefore the trained
-# model is not independent of the test set anymore (even though they
-# were never used for backpropagation!).
-# This is why you need a third split, the so called validation set, if
-# you perform any hyperparameter tuning.
+# Deep learning models usually have many free parameters; they could
+# be considered complex models with many degrees of freedom.
+# If you kept using the test dataset to evaluate your adjustment
+# you would run into data leakage.
+#
+# This means that if you use the test set to adjust the hyperparameters
+# of your model, the model implicitly learns or memorizes the test set.
+# Therefore, the trained model is no longer independent of the test set
+# (even though they were never used for backpropagation!).
+# If you perform any Hyperparameter tuning, you need a third split,
+# the so-called validation set.
+#
 # This tutorial shows two different methods (Option 2 and 3) to do
 # hyperparameter tuning.
-# Option 1 shows how to train with a 2-fold split (train and test,
+# Option 1 presents how to train with a 2-fold split (train and test,
 # no validation split). Option 1 should only be used if you already
 # know your hyperparameter configuration.
 #
@@ -59,8 +68,10 @@ dataset = MOABBDataset(dataset_name="BNCI2014001", subject_ids=[subject_id])
 # ~~~~~~~~~~~~~
 #
 
+import numpy as np
+
 from braindecode.preprocessing import (
-    exponential_moving_standardize, preprocess, Preprocessor, scale)
+    exponential_moving_standardize, preprocess, Preprocessor)
 
 low_cut_hz = 4.  # low cut frequency for filtering
 high_cut_hz = 38.  # high cut frequency for filtering
@@ -70,7 +81,8 @@ init_block_size = 1000
 
 preprocessors = [
     Preprocessor('pick_types', eeg=True, meg=False, stim=False),  # Keep EEG sensors
-    Preprocessor(scale, factor=1e6, apply_on_array=True),  # Convert from V to uV
+    Preprocessor(lambda data, factor: np.multiply(data, factor),  # Convert from V to uV
+                 factor=1e6),
     Preprocessor('filter', l_freq=low_cut_hz, h_freq=high_cut_hz),  # Bandpass filter
     Preprocessor(exponential_moving_standardize,  # Exponential moving standardization
                  factor_new=factor_new, init_block_size=init_block_size)
@@ -112,9 +124,9 @@ windows_dataset = create_windows_from_events(
 
 ######################################################################
 # We can easily split the dataset using additional info stored in the
-# description attribute, in this case ``session`` column. We select
-# ``session_T`` for training and ``session_E`` for validation.
-# For other datasets you might have to choose another column.
+# description attribute, in this case the ``session`` column. We
+# select ``session_T`` for training and ``session_E`` for validation.
+# For other datasets, you might have to choose another column.
 #
 # .. note::
 #    No matter which of the 3 validation schemes you use, this initial
@@ -169,8 +181,8 @@ if cuda:
 
 ######################################################################
 # This is the easiest training scheme to use as the dataset is only
-# splitted in two distinct sets (``train_set`` and ``test_set``).
-# As this method uses no separate validation split it should only be
+# split into two distinct sets (``train_set`` and ``test_set``).
+# This scheme uses no separate validation split and should only be
 # used for the final evaluation of the (previously!) found
 # hyperparameters configuration.
 #
@@ -219,12 +231,12 @@ print(f"Test acc: {(test_acc * 100):.2f}%")
 #
 
 ######################################################################
-# Usually when developing a new Deep Learning model/method finding
+# Usually when developing a new deep learning model/method finding
 # the best (or at least a suitable) hyperparameter configuration makes
-# up a substantial part of the developement process.
+# up a substantial part of the development process.
 # As stated above, it is not suitable to use the ``test_set``
 # for this hyperparamter search. Therefore we need a third split, the
-# so called validation set which is a Subset of the ``train_set``.
+# so-called validation set which is a Subset of the ``train_set``.
 # This second option splits the original ``train_set`` only once
 # (instead of k times as in Option 3) to speed up the tuning process.
 # This method should only be preferred over Option 3 if either the
@@ -267,7 +279,7 @@ clf = EEGClassifier(
 # We will make use of the sklearn library to do the hyperparameter
 # search. The ``train_test_split`` function will split the ``train_set``
 # into two sets. We can specify the ratio of the split via the
-# ``test_size`` parameter. Here we use a 80-20 train-val split.
+# ``test_size`` parameter. Here we use an 80-20 train-validation split.
 #
 # .. note::
 #    The parameter ``shuffle`` is set to ``False``. For time-series
@@ -276,7 +288,6 @@ clf = EEGClassifier(
 #    performance less significant.
 #
 
-import numpy as np
 import pandas as pd
 from sklearn.model_selection import GridSearchCV, KFold, train_test_split
 from skorch.helper import SliceDataset
@@ -316,10 +327,47 @@ print(f"Best hyperparameters were {best_run['params']} which gave a validation "
       f"accuracy of {best_run['mean_test_score'] * 100:.2f}% (training "
       f"accuracy of {best_run['mean_train_score'] * 100:.2f}%).")
 
-X_test = SliceDataset(test_set, idx=0)
-y_test = SliceDataset(test_set, idx=1)
-test_acc = search.score(X_test, y_test)
-print(f"Test accuracy: {test_acc * 100:.2f}%.")
+######################################################################
+# .. note::
+#    Here we evaluate the best hyperparamter configuration on
+#    the validation set. If you are happy with the results of your
+#    hyperparameter search you can use option 1 to evaluate it on the
+#    test set.
+#
+# If you want to adjust your hyperparameters manually instead of via
+# using a dedicated search strategy you can make use of the
+# ``train_split`` argument of ``EEGClassifier``. If you leave this empty
+# (not None!), skorch will make an 80-20 train-validation split.
+# If you want to control the split manually you can do that by using
+# ``Subset`` from torch and ``predefined_split`` from skorch.
+#
+
+from torch.utils.data import Subset
+from skorch.helper import predefined_split
+
+train_indices, val_indices = train_val_split[0]
+train_subset = Subset(train_set, train_indices)
+val_subset = Subset(train_set, val_indices)
+
+clf = EEGClassifier(
+    model,
+    criterion=torch.nn.NLLLoss,
+    optimizer=torch.optim.AdamW,
+    train_split=predefined_split(val_subset),
+    optimizer__lr=lr,
+    optimizer__weight_decay=weight_decay,
+    batch_size=batch_size,
+    callbacks=[
+        "accuracy", ("lr_scheduler", LRScheduler('CosineAnnealingLR', T_max=n_epochs - 1)),
+    ],
+    device=device,
+)
+clf.fit(train_subset, y=None, epochs=n_epochs)
+
+# score the Model after training
+y_test = test_set.get_metadata().target
+test_acc = clf.score(test_set, y=y_test)
+print(f"Test acc: {(test_acc * 100):.2f}%")
 
 ######################################################################
 # Option 3: Hyperparameter Search via Cross Validation
@@ -329,15 +377,51 @@ print(f"Test accuracy: {test_acc * 100:.2f}%.")
 ######################################################################
 # As mentioned above, using only one validation split might not be
 # sufficient, as there might be a shift in the data distribution.
-# To compensate this, one can run a k-fold Cross Validation, where
-# every sample of the training set is in the validation set once.
-# By averaging over the k validation scores afterwards, you get a
+# To compensate for this, one can run a k-fold Cross Validation,
+# where every sample of the training set is in the validation set once.
+# After averaging over the k validation scores afterwards, you get a
 # very reliable estimate of how the model would perform on unseen
 # data (test set).
-# The implementation is straight forward as we only have to change one
+# The implementation is straightforward as we only have to change one
 # line of code compared to Option 2. The ``KFold`` class will split
-# the ``train_set`` into k folds. We will use 5 folds to get a 80-20
-# train-val split.
+# the ``train_set`` into k folds. We will use 5 folds to get an 80-20
+# train-validation split.
 #
 
 train_val_split = KFold(n_splits=5, shuffle=False)
+
+######################################################################
+# For manual hyperparameter search you can make use of the sklearn
+# function `cross_val_score`.
+
+from sklearn.model_selection import cross_val_score
+
+clf = EEGClassifier(
+    model,
+    criterion=torch.nn.NLLLoss,
+    optimizer=torch.optim.AdamW,
+    train_split=None,
+    optimizer__lr=lr,
+    optimizer__weight_decay=weight_decay,
+    batch_size=batch_size,
+    callbacks=[
+        "accuracy", ("lr_scheduler", LRScheduler('CosineAnnealingLR', T_max=n_epochs - 1)),
+    ],
+    device=device,
+)
+
+train_val_split = KFold(n_splits=5, shuffle=False)
+cv_results = cross_val_score(
+    clf, X_train, y_train, scoring='accuracy', cv=train_val_split, fit_params=fit_params)
+print(f"Validation accuracy: {np.mean(cv_results*100):.2f}"
+      f"+-{np.std(cv_results*100):.2f}%")
+
+######################################################################
+# .. note::
+#    You might have recognized that the accuracy got better throughout
+#    the experiments of this tutorial. The reason behind that is that
+#    we always use the same model with the same paramters in every
+#    segment to keep the tutorial short and readable. If you do your
+#    own experiments you always have to reinitialize the model before
+#    training.
+#
