286792d2f2629ab7e37b5371b1954dbe7ea777ac
tgnassou <66993815+tgnassou@users.noreply.github.com>
2022-10-06 17:29:36 +0200

    clean definition
    Co-authored-by: Alexandre Gramfort <alexandre.gramfort@m4x.org>

--- braindecode/models/deepsleepnet.py
+++ braindecode/models/deepsleepnet.py
@@ -3,10 +3,7 @@ import torch.nn as nn
 
 
 class _SmallCNN(nn.Module):  # smaller filter sizes to learn temporal information
-    def __init__(
-        self,
-    ):
-
+    def __init__(self):
         super().__init__()
         self.conv1 = nn.Sequential(
             nn.Conv2d(
