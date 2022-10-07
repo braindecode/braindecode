6c874a40ea7f6b8e0786e21fd115afd4ec676af1
Théo <theo.gnassounou@gmail.com>
2022-10-07 10:37:39 +0200

    update test to take into account 4D and 3D inputs

--- test/unit_tests/models/test_models.py
+++ test/unit_tests/models/test_models.py
@@ -360,9 +360,12 @@ def test_deepsleepnet(n_classes):
     X = rng.randn(n_examples, n_channels, np.ceil(input_size_s * sfreq).astype(int))
     X = torch.from_numpy(X.astype(np.float32))
 
-    y_pred = model(X.unsqueeze(2))  # 4D inputs
-    assert y_pred.shape == (n_examples, n_classes)
-    np.testing.assert_allclose(y_pred.detach().cpu().numpy())
+    y_pred1 = model(X)  # 3D inputs
+    y_pred2 = model(X.unsqueeze(1))  # 4D inputs
+    assert y_pred1.shape == (n_examples, n_classes)
+    assert y_pred2.shape == (n_examples, n_classes)
+    np.testing.assert_allclose(y_pred1.detach().cpu().numpy(),
+                               y_pred2.detach().cpu().numpy())
 
 
 def test_deepsleepnet_feats():
@@ -379,5 +382,5 @@ def test_deepsleepnet_feats():
     X = rng.randn(n_examples, n_channels, int(sfreq * input_size_s))
     X = torch.from_numpy(X.astype(np.float32))
 
-    out = model(X.unsqueeze(2))
+    out = model(X.unsqueeze(1))
     assert out.shape == (n_examples, model.len_last_layer)
