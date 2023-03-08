2616e4c25a7b23033e5691cfa14c27fad9f67046
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2023-03-08 19:48:15 +0100

    Adding new function into model utils braindecode

--- braindecode/models/__init__.py
+++ braindecode/models/__init__.py
@@ -17,5 +17,5 @@ from .sleep_stager_blanco_2020 import SleepStagerBlanco2020
 from .sleep_stager_eldele_2021 import SleepStagerEldele2021
 from .tidnet import TIDNet
 from .usleep import USleep
-from .util import get_output_shape, to_dense_prediction_model
+from .util import get_output_shape, to_dense_prediction_model, check_deprecation_warning
 from .modules import TimeDistributed
