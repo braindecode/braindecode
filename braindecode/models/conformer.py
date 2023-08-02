d423e901bf8588bc75824b87ed4699dab1bb9398
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2023-08-02 16:35:21 +0200

    Reformarting model and adding more information

--- braindecode/models/conformer.py
+++ braindecode/models/conformer.py
@@ -2,23 +2,13 @@
 #
 # License: BSD (3-clause)
 
-import os
-import argparse
-import numpy as np
 import math
 
 import torch
-from torch import nn
-from torch import Tensor
 import torch.nn.functional as F
-from torch.autograd import Variable
-
 from einops import rearrange
 from einops.layers.torch import Rearrange, Reduce
-
-from torch.backends import cudnn
-cudnn.benchmark = False
-cudnn.deterministic = True
+from torch import nn, Tensor
 
 
 class Conformer(nn.Sequential):
@@ -65,30 +55,47 @@ class Conformer(nn.Sequential):
     - n_classes: number of classes
 
     """
-    def __init__(self, 
-                 kernel=40,
-                 kernel_temp_conv=25,
-                 eeg_channel=22, 
-                 kernel_avg_pool=75,
-                 stride_avg_pool=15,
-                 conv_drop=0.5,
-                 att_depth=6, 
-                 att_heads=10,
-                 att_drop=0.5,
-                 fc_dim=2440,
-                 n_classes=4, 
-                 **kwargs):
-        super().__init__(
 
-            PatchEmbedding(kernel, kernel_temp_conv, eeg_channel, kernel_avg_pool, stride_avg_pool, conv_drop),
+    def __init__(
+        self,
+        kernel=40,
+        kernel_temp_conv=25,
+        eeg_channel=22,
+        kernel_avg_pool=75,
+        stride_avg_pool=15,
+        conv_drop=0.5,
+        att_depth=6,
+        att_heads=10,
+        att_drop=0.5,
+        fc_dim=2440,
+        n_classes=4,
+    ):
+        super().__init__(
+            PatchEmbedding(
+                kernel,
+                kernel_temp_conv,
+                eeg_channel,
+                kernel_avg_pool,
+                stride_avg_pool,
+                conv_drop,
+            ),
             TransformerEncoder(att_depth, kernel, att_heads, att_drop),
-            ClassificationHead(kernel, fc_dim, n_classes)
+            ClassificationHead(kernel, fc_dim, n_classes),
         )
+
 
 # Convolution module
 # use conv to capture local features, instead of postion embedding.
 class PatchEmbedding(nn.Module):
-    def __init__(self, kernel, kernel_temp_conv, eeg_channel, kernel_avg_pool, stride_avg_pool, conv_drop):
+    def __init__(
+        self,
+        kernel,
+        kernel_temp_conv,
+        eeg_channel,
+        kernel_avg_pool,
+        stride_avg_pool,
+        conv_drop,
+    ):
         # self.patch_size = patch_size
         super().__init__()
 
@@ -97,18 +104,22 @@ class PatchEmbedding(nn.Module):
             nn.Conv2d(kernel, kernel, (eeg_channel, 1), (1, 1)),
             nn.BatchNorm2d(kernel),
             nn.ELU(),
-            nn.AvgPool2d((1, kernel_avg_pool), (1, stride_avg_pool)),  # pooling acts as slicing to obtain 'patch' along the time dimension as in ViT
+            nn.AvgPool2d(
+                (1, kernel_avg_pool), (1, stride_avg_pool)
+            ),
+            # pooling acts as slicing to obtain 'patch' along the
+            # time dimension as in ViT
             nn.Dropout(conv_drop),
         )
 
         self.projection = nn.Sequential(
-            nn.Conv2d(kernel, kernel, (1, 1), stride=(1, 1)),  # transpose, conv could enhance fiting ability slightly
-            Rearrange('b e (h) (w) -> b (h w) e'),
+            nn.Conv2d(
+                kernel, kernel, (1, 1), stride=(1, 1)
+            ),  # transpose, conv could enhance fiting ability slightly
+            Rearrange("b e (h) (w) -> b (h w) e"),
         )
 
-
     def forward(self, x: Tensor) -> Tensor:
-        # x = x.unsqueeze(dim=1)
         x = self.shallownet(x)
         x = self.projection(x)
         return x
@@ -126,10 +137,16 @@ class MultiHeadAttention(nn.Module):
         self.projection = nn.Linear(emb_size, emb_size)
 
     def forward(self, x: Tensor, mask: Tensor = None) -> Tensor:
-        queries = rearrange(self.queries(x), "b n (h d) -> b h n d", h=self.num_heads)
-        keys = rearrange(self.keys(x), "b n (h d) -> b h n d", h=self.num_heads)
-        values = rearrange(self.values(x), "b n (h d) -> b h n d", h=self.num_heads)
-        energy = torch.einsum('bhqd, bhkd -> bhqk', queries, keys)  
+        queries = rearrange(
+            self.queries(x), "b n (h d) -> b h n d", h=self.num_heads
+        )
+        keys = rearrange(
+            self.keys(x), "b n (h d) -> b h n d", h=self.num_heads
+        )
+        values = rearrange(
+            self.values(x), "b n (h d) -> b h n d", h=self.num_heads
+        )
+        energy = torch.einsum("bhqd, bhkd -> bhqk", queries, keys)
         if mask is not None:
             fill_value = torch.finfo(torch.float32).min
             energy.mask_fill(~mask, fill_value)
@@ -137,7 +154,7 @@ class MultiHeadAttention(nn.Module):
         scaling = self.emb_size ** (1 / 2)
         att = F.softmax(energy / scaling, dim=-1)
         att = self.att_drop(att)
-        out = torch.einsum('bhal, bhlv -> bhav ', att, values)
+        out = torch.einsum("bhal, bhlv -> bhav ", att, values)
         out = rearrange(out, "b h n d -> b n (h d)")
         out = self.projection(out)
         return out
@@ -167,44 +184,50 @@ class FeedForwardBlock(nn.Sequential):
 
 class GELU(nn.Module):
     def forward(self, input: Tensor) -> Tensor:
-        return input*0.5*(1.0+torch.erf(input/math.sqrt(2.0)))
+        return input * 0.5 * (1.0 + torch.erf(input / math.sqrt(2.0)))
 
 
 class TransformerEncoderBlock(nn.Sequential):
-    def __init__(self,
-                 emb_size,
-                 att_heads,
-                 att_drop,
-                 forward_expansion=4):
+    def __init__(self, emb_size, att_heads, att_drop, forward_expansion=4):
         super().__init__(
-            ResidualAdd(nn.Sequential(
-                nn.LayerNorm(emb_size),
-                MultiHeadAttention(emb_size, att_heads, att_drop),
-                nn.Dropout(att_drop)
-            )),
-            ResidualAdd(nn.Sequential(
-                nn.LayerNorm(emb_size),
-                FeedForwardBlock(
-                    emb_size, expansion=forward_expansion, drop_p=att_drop),
-                nn.Dropout(att_drop)
-            )
-            ))
+            ResidualAdd(
+                nn.Sequential(
+                    nn.LayerNorm(emb_size),
+                    MultiHeadAttention(emb_size, att_heads, att_drop),
+                    nn.Dropout(att_drop),
+                )
+            ),
+            ResidualAdd(
+                nn.Sequential(
+                    nn.LayerNorm(emb_size),
+                    FeedForwardBlock(
+                        emb_size, expansion=forward_expansion, drop_p=att_drop
+                    ),
+                    nn.Dropout(att_drop),
+                )
+            ),
+        )
 
 
 class TransformerEncoder(nn.Sequential):
     def __init__(self, att_depth, emb_size, att_heads, att_drop):
-        super().__init__(*[TransformerEncoderBlock(emb_size, att_heads, att_drop) for _ in range(att_depth)])
+        super().__init__(
+            *[
+                TransformerEncoderBlock(emb_size, att_heads, att_drop)
+                for _ in range(att_depth)
+            ]
+        )
 
 
 class ClassificationHead(nn.Sequential):
     def __init__(self, emb_size, fc_dim, n_classes):
         super().__init__()
-        
+
         # global average pooling
         self.clshead = nn.Sequential(
-            Reduce('b n e -> b e', reduction='mean'),
+            Reduce("b n e -> b e", reduction="mean"),
             nn.LayerNorm(emb_size),
-            nn.Linear(emb_size, n_classes)
+            nn.Linear(emb_size, n_classes),
         )
         self.fc = nn.Sequential(
             nn.Linear(fc_dim, 256),
@@ -213,7 +236,7 @@ class ClassificationHead(nn.Sequential):
             nn.Linear(256, 32),
             nn.ELU(),
             nn.Dropout(0.3),
-            nn.Linear(32, n_classes)
+            nn.Linear(32, n_classes),
         )
 
     def forward(self, x):
@@ -221,36 +244,3 @@ class ClassificationHead(nn.Sequential):
         out = self.fc(x)
         return x, out
 
-
-# # Segmentation and Reconstruction (S&R) data augmentation
-# def interaug(self, timg, label, channels, samples):  
-#     aug_data = []
-#     aug_label = []
-#     for cls4aug in range(4): # 4 is the number of classes
-#         cls_idx = np.where(label == cls4aug + 1)
-#         tmp_data = timg[cls_idx]
-#         tmp_label = label[cls_idx]
-
-#         tmp_aug_data = np.zeros((int(self.batch_size / 4), 1, channels, samples))
-#         # segment the original signal into 8 segments (1000/8=125) along the time dimension and randomly select one segment for each sample
-#         for ri in range(int(self.batch_size / 4)):
-#             for rj in range(8):
-#                 rand_idx = np.random.randint(0, tmp_data.shape[0], 8)
-#                 tmp_aug_data[ri, :, :, rj * 125:(rj + 1) * 125] = tmp_data[rand_idx[rj], :, :,
-#                                                                     rj * 125:(rj + 1) * 125]
-                
-#         aug_data.append(tmp_aug_data)
-#         aug_label.append(tmp_label[:int(self.batch_size / 4)])
-#     aug_data = np.concatenate(aug_data)
-#     aug_label = np.concatenate(aug_label)
-
-#     # shuffle the augment data
-#     aug_shuffle = np.random.permutation(len(aug_data))
-#     aug_data = aug_data[aug_shuffle, :, :]
-#     aug_label = aug_label[aug_shuffle]
-
-#     aug_data = torch.from_numpy(aug_data).cuda()
-#     aug_data = aug_data.float()
-#     aug_label = torch.from_numpy(aug_label-1).cuda()
-#     aug_label = aug_label.long()
-#     return aug_data, aug_label
