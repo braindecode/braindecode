ecac7aa847ab338a9333495fb3e3d8aeb608a8a8
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2023-08-31 21:51:33 +0200

    Adding skipif

--- test/acceptance_tests/test_cropped_decoding.py
+++ test/acceptance_tests/test_cropped_decoding.py
@@ -2,6 +2,8 @@
 #          Robin Tibor Schirrmeister
 #
 # License: BSD-3
+import sys
+import pytest
 
 import mne
 import numpy as np
@@ -18,6 +20,7 @@ from braindecode.models.util import to_dense_prediction_model, get_output_shape
 from braindecode.util import set_random_seeds
 
 
+@pytest.mark.skipif(sys.version_info != (3, 7), reason="Only for Python 3.7")
 def test_cropped_decoding():
     # 5,6,7,10,13,14 are codes for executed and imagined hands/feet
     subject_id = 1
@@ -125,8 +128,8 @@ def test_cropped_decoding():
                 1.111006
             ]
         ),
-        atol=2.5E-1,
-        rtol=2E-1  # Fixing this latter
+        rtol=1e-3,
+        atol=1e-4,
     )
 
     np.testing.assert_allclose(
@@ -139,8 +142,8 @@ def test_cropped_decoding():
                 0.933025
             ]
         ),
-        atol=2.5E-1,
-        rtol=2E-1  # Fixing this latter
+        rtol=1e-3,
+        atol=1e-4,
     )
 
     np.testing.assert_allclose(
