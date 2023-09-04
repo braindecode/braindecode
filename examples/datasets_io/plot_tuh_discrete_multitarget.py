847c4411323607bfcf614ca7ad75c95b903c22f4
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2023-09-04 19:45:48 +0200

    Improve the Temple university dataset example

--- examples/datasets_io/plot_tuh_discrete_multitarget.py
+++ examples/datasets_io/plot_tuh_discrete_multitarget.py
@@ -2,8 +2,8 @@
 Multiple discrete targets with the TUH EEG Corpus
 =================================================
 
-In this example, we showcase usage of multiple discrete targets per recording
-with the TUH EEG Corpus.
+Welcome to this tutorial where we demonstrate how to work with multiple discrete targets for each recording in the TUH EEG Corpus. We'll guide you through the process step by step.
+
 """
 
 # Author: Lukas Gemein <l.gemein@gmail.com>
@@ -16,6 +16,10 @@ from torch.utils.data import DataLoader
 from braindecode.datasets import TUH
 from braindecode.preprocessing import create_fixed_length_windows
 
+## Setting Logging Level
+# We'll set the logging level to 'ERROR' to avoid excessive messages when
+# extracting windows:
+
 mne.set_log_level('ERROR')  # avoid messages everytime a window is extracted
 
 
@@ -27,7 +31,10 @@ from braindecode.datasets.tuh import _TUHMock as TUH  # noqa F811
 
 
 ###############################################################################
-# We start by creating a TUH dataset. Instead of just a str, we give it
+# Creating Temple University Hospital (TUH) EEG Corpus Dataset
+# ------------------------------------------------------------
+#
+# We start by creating a TUH dataset. Instead of just a `str, we give it
 # multiple strings as target names. Each of the strings has to exist as a
 # column in the description DataFrame.
 
@@ -39,18 +46,28 @@ tuh = TUH(
     preload=False,
     add_physician_reports=False,
 )
-tuh.description
+print(tuh.description)
+
 
 
 ###############################################################################
-# Iterating through the dataset gives x as ndarray(n_channels x 1) as well as
-# the target as [age of the subject, gender of the subject]. Let's look at the last example
-# as it has more interesting age/gender labels (compare to the last row of the dataframe above).
+# Exploring Data
+# --------------
+#
+# Iterating through the dataset gives `x` as an ndarray with shape
+# `(n_channels x 1)` and `y` as a list containing `[age of the subject, gender
+# of the subject]`.
+# Let's look at the last example as it has more interesting age/gender labels
+# (compare to the last row of the dataframe above).
 x, y = tuh[-1]
+
 print(f'{x=}\n{y=}')
 
 
 ###############################################################################
+# Creating Windows
+# ----------------
+#
 # We will skip preprocessing steps for now, since it is not the aim of this
 # example. Instead, we will directly create compute windows. We specify a
 # mapping from genders 'M' and 'F' to integers, since this is required for
@@ -71,13 +88,20 @@ tuh_windows.set_description({
 
 
 ###############################################################################
-# Iterating through the dataset gives x as ndarray(n_channels x 1000), y as
-# [age, gender], and ind. Let's look at the last example again.
+# Exploring Windows
+# -----------------
+#
+# Iterating through the dataset gives `x` as an ndarray with shape
+# `(n_channels x 1000)`, `y` as `[age, gender]`, and `ind`.
+# Let's look at the last example again.
 x, y, ind = tuh_windows[-1]
 print(f'{x=}\n{y=}\n{ind=}')
 
 
 ###############################################################################
+# DataLoader for Model Training
+# -----------------------------
+#
 # We give the dataset to a pytorch DataLoader, such that it can be used for
 # model training.
 dl = DataLoader(
@@ -87,10 +111,15 @@ dl = DataLoader(
 
 
 ###############################################################################
-# Iterating through the DataLoader gives batch_X as tensor(4 x n_channels x
-# 1000), batch_y as [tensor([4 x age of subject]), tensor([4 x gender of
-# subject])], and batch_ind. We will iterate to the end to look at the last example
-# again.
+# Exploring DataLoader
+# --------------------
+#
+# When iterating through the DataLoader, we get `batch_X` as a tensor with shape
+# `(4 x n_channels x 1000)`, `batch_y` as `[tensor([4 x age of subject]),
+# tensor([4 x gender of subject])]`, and `batch_ind`. To view the last example,
+# simply iterate through the DataLoader:
+
 for batch_X, batch_y, batch_ind in dl:
     pass
+
 print(f'{batch_X=}\n{batch_y=}\n{batch_ind=}')
