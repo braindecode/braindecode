25033f70948009ef2f1777937fcf3ad2ed0a7678
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2023-09-06 13:38:52 +0200

    Fixing again

--- braindecode/visualization/__init__.py
+++ braindecode/visualization/__init__.py
@@ -1,6 +1,6 @@
 """
 Functions for visualisations, especially of the ConvNets.
 """
-from .confusion_matrices import plot_confusion_matrix  # noqa
 # flake8: noqa
+from .confusion_matrices import plot_confusion_matrix  # noqa
 from .gradients import compute_amplitude_gradients  # noqa
