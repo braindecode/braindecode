9e98a1375da3a9ce7fb3dd86ed25b6534b36d416
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2024-02-14 00:26:31 +0100

    exposing to init

--- braindecode/models/__init__.py
+++ braindecode/models/__init__.py
@@ -13,6 +13,7 @@ from .eegresnet import EEGResNet
 from .eeginception import EEGInception
 from .eeginception_erp import EEGInceptionERP
 from .eeginception_mi import EEGInceptionMI
+from .lmda import LMDA
 from .atcnet import ATCNet
 from .tcn import TCN
 from .sleep_stager_chambon_2018 import SleepStagerChambon2018
