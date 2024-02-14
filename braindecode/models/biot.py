3ee8fcd2c320c211f215b567a70eda9b846da6c0
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2024-02-14 23:02:53 +0100

    draft

---
+++ braindecode/models/biot.py
@@ -0,0 +1,227 @@
+import math
+
+import torch
+import torch.nn as nn
+import numpy as np
+from linear_attention_transformer import LinearAttentionTransformer
+
+
+class PatchFrequencyEmbedding(nn.Module):
+    def __init__(self, emb_size=256, n_freq=101):
+        super().__init__()
+        self.projection = nn.Linear(n_freq, emb_size)
+
+    def forward(self, x):
+        """
+        x: (batch, freq, time)
+        out: (batch, time, emb_size)
+        """
+        x = x.permute(0, 2, 1)
+        x = self.projection(x)
+        return x
+
+
+class ClassificationHead(nn.Sequential):
+    def __init__(self, emb_size, n_classes):
+        super().__init__()
+        self.clshead = nn.Sequential(
+            nn.ELU(),
+            nn.Linear(emb_size, n_classes),
+        )
+
+    def forward(self, x):
+        out = self.clshead(x)
+        return out
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 1000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        # Compute the positional encodings once in log space.
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len).unsqueeze(1).float()
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model)
+        )
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x: torch.FloatTensor) -> torch.FloatTensor:
+        """
+        Args:
+            x: `embeddings`, shape (batch, max_len, d_model)
+        Returns:
+            `encoder input`, shape (batch, max_len, d_model)
+        """
+        x = x + self.pe[:, : x.size(1)]
+        return self.dropout(x)
+
+
+class BIOTEncoder(nn.Module):
+    def __init__(
+        self,
+        emb_size=256,
+        heads=8,
+        depth=4,
+        n_channels=16,
+        n_fft=200,
+        hop_length=100,
+    ):
+        super().__init__()
+
+        self.n_fft = n_fft
+        self.hop_length = hop_length
+
+        self.patch_embedding = PatchFrequencyEmbedding(
+            emb_size=emb_size, n_freq=self.n_fft // 2 + 1
+        )
+        self.transformer = LinearAttentionTransformer(
+            dim=emb_size,
+            heads=heads,
+            depth=depth,
+            max_seq_len=1024,
+            attn_layer_dropout=0.2,  # dropout right after self-attention layer
+            attn_dropout=0.2,  # dropout post-attention
+        )
+        self.positional_encoding = PositionalEncoding(emb_size)
+
+        # channel token, N_channels >= your actual channels
+        self.channel_tokens = nn.Embedding(n_channels, 256)
+        self.index = nn.Parameter(
+            torch.LongTensor(range(n_channels)), requires_grad=False
+        )
+
+    def stft(self, sample):
+        spectral = torch.stft(
+            input = sample.squeeze(1),
+            n_fft = self.n_fft,
+            hop_length = self.hop_length,
+            center = False,
+            onesided = True,
+            return_complex = True,
+        )
+        return torch.abs(spectral)
+
+    def forward(self, x, n_channel_offset=0, perturb=False):
+        """
+        x: [batch_size, channel, ts]
+        output: [batch_size, emb_size]
+        """
+        emb_seq = []
+        for i in range(x.shape[1]):
+            channel_spec_emb = self.stft(x[:, i : i + 1, :])
+            channel_spec_emb = self.patch_embedding(channel_spec_emb)
+            batch_size, ts, _ = channel_spec_emb.shape
+            # (batch_size, ts, emb)
+            channel_token_emb = (
+                self.channel_tokens(self.index[i + n_channel_offset])
+                .unsqueeze(0)
+                .unsqueeze(0)
+                .repeat(batch_size, ts, 1)
+            )
+            # (batch_size, ts, emb)
+            channel_emb = self.positional_encoding(channel_spec_emb + channel_token_emb)
+
+            # perturb
+            if perturb:
+                ts = channel_emb.shape[1]
+                ts_new = np.random.randint(ts // 2, ts)
+                selected_ts = np.random.choice(range(ts), ts_new, replace=False)
+                channel_emb = channel_emb[:, selected_ts]
+            emb_seq.append(channel_emb)
+
+        # (batch_size, 16 * ts, emb)
+        emb = torch.cat(emb_seq, dim=1)
+        # (batch_size, emb)
+        emb = self.transformer(emb).mean(dim=1)
+        return emb
+
+
+class BIOTClassifier(nn.Module):
+    """BIOT: Cross-data Biosignal Learning in the Wild.
+
+        The paper and original code with more details about the methodological
+        choices are available at the [Song2022]_ and [ConformerCode]_.
+
+    This neural network architecture receives a traditional braindecode input.
+    The input shape should be three-dimensional matrix representing the EEG
+    signals.
+
+         `(batch_size, n_channels, n_timesteps)`.
+
+    The EEG Conformer architecture is composed of three modules:
+        - PatchEmbedding
+        - TransformerEncoder
+        - ClassificationHead
+
+    Notes
+    -----
+    The authors recommend using data augmentation before using Conformer,
+    e.g. segmentation and recombination,
+    Please refer to the original paper and code for more details.
+
+    The model was initially tuned on 4 seconds of 250 Hz data.
+    Please adjust the scale of the temporal convolutional layer,
+    and the pooling layer for better performance.
+
+    .. versionadded:: 0.8
+
+    We aggregate the parameters based on the parts of the models, or
+    when the parameters were used first, e.g. n_filters_time.
+
+    Parameters
+    ----------
+    n_filters_time: int
+        Number of temporal filters, defines also embedding size.
+    filter_time_length: int
+        Length of the temporal filter.
+    pool_time_length: int
+        Length of temporal pooling filter.
+    pool_time_stride: int
+        Length of stride between temporal pooling filters.
+    drop_prob: float
+        Dropout rate of the convolutional layer.
+    att_depth: int
+        Number of self-attention layers.
+    att_heads: int
+        Number of attention heads.
+    att_drop_prob: float
+        Dropout rate of the self-attention layer.
+    final_fc_length: int | str
+        The dimension of the fully connected layer.
+    return_features: bool
+        If True, the forward method returns the features before the
+        last classification layer. Defaults to False.
+    n_classes :
+        Alias for n_outputs.
+    n_channels :
+        Alias for n_chans.
+    input_window_samples :
+        Alias for n_times.
+    References
+    ----------
+    .. [Song2022] Song, Y., Zheng, Q., Liu, B. and Gao, X., 2022. EEG
+       conformer: Convolutional transformer for EEG decoding and visualization.
+       IEEE Transactions on Neural Systems and Rehabilitation Engineering,
+       31, pp.710-719. https://ieeexplore.ieee.org/document/9991178
+    .. [ConformerCode] Song, Y., Zheng, Q., Liu, B. and Gao, X., 2022. EEG
+       conformer: Convolutional transformer for EEG decoding and visualization.
+       https://github.com/eeyhsong/EEG-Conformer.
+    """
+
+
+    def __init__(self, emb_size=256, heads=8, depth=4, n_classes=6):
+        super().__init__()
+        self.biot = BIOTEncoder(emb_size=emb_size,
+                                heads=heads,
+                                depth=depth)
+        self.classifier = ClassificationHead(emb_size, n_classes)
+
+    def forward(self, x):
+        x = self.biot(x)
+        x = self.classifier(x)
+        return x
