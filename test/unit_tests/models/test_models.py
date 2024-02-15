c44ad5dd09df7917c32da82574dfc05d2e9d8be5
Bru <a.bruno@aluno.ufabc.edu.br>
2024-02-15 14:36:18 +0000

    Update test/unit_tests/models/test_models.py

--- test/unit_tests/models/test_models.py
+++ test/unit_tests/models/test_models.py
@@ -479,7 +479,7 @@ def test_eldele_2021(sfreq, n_classes, input_size_s, d_model):
     # (100, 5, 30, 80) - Physionet Sleep
     # (125, 4, 30, 100) - SHHS
     rng = np.random.RandomState(42)
-    n_channels = 10
+    n_channels = 1
     n_examples = 10
 
     model = SleepStagerEldele2021(
