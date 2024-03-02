30186aa8f0b735614ce338fa0ca2bcb32170a78e
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2024-03-02 20:53:56 +0100

    Adding labram

--- braindecode/models/__init__.py
+++ braindecode/models/__init__.py
@@ -24,6 +24,7 @@ from .usleep import USleep
 from .util import get_output_shape, to_dense_prediction_model
 from .modules import TimeDistributed
 from .util import _init_models_dict, models_mandatory_parameters
+from .labram import Labram
 
 # Call this last in order to make sure the dataset list is populated with
 # the models imported in this file.
