02505668964aa47027bedffb22853681c18d4578
Martin Wimpff <42936990+martinwimpff@users.noreply.github.com>
2024-03-28 14:02:00 +0100

    Update braindecode/models/functions_attention.py
    Co-authored-by: Bru <a.bruno@aluno.ufabc.edu.br>

--- braindecode/models/functions_attention.py
+++ braindecode/models/functions_attention.py
@@ -47,7 +47,7 @@ def _get_gaussian_kernel1d(kernel_size: int, sigma: float) -> torch.Tensor:
         copyright notice, this list of conditions and the following
         disclaimer in the documentation and/or other materials provided
         with the distribution.
-        * Neither the name of the NumPy Developers nor the names of any
+        * Neither the name of the Pytorch Developers nor the names of any
         contributors may be used to endorse or promote products derived
         from this software without specific prior written permission.
 
