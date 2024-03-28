f98fa1778791716a0c8f0a7630642018c854af8a
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2024-03-28 16:12:38 +0100

    improving the doc a little

--- braindecode/models/eegsimpleconv.py
+++ braindecode/models/eegsimpleconv.py
@@ -1,3 +1,9 @@
+"""
+EEG Simple Convolutional Neural Network from Yassine El Ouahidi et al. (2023).
+
+Each EEG channels is convoled individually.
+
+"""
 # Authors: Yassine El Ouahidi <eloua.yas@gmail.com>
 #
 # License: BSD-3
@@ -9,41 +15,61 @@ from .base import EEGModuleMixin
 
 
 class EEGSimpleConv(EEGModuleMixin, torch.nn.Module):
-    """
-    EEGSimpleConv. Model described in [1]. Original code from: https://github.com/elouayas/EEGSimpleConv
+    """EEGSimpleConv from [Yassine2023]_.
+
+    Can you increase the description here Yassine?
+
+    The paper and original code with more details about the methodological
+    choices are available at the [Yassine2023]_ and [Yassine2023Code]_.
+
+    Notes
+    -----
+    The authors recommend using the default parameters for MI decoding.
+    Please refer to the original paper and code for more details.
+
+    An intesive ablation study is available in the paper to understand the
+    impact of each parameter on the model performance.
+
+    .. versionadded:: 0.9
 
     Parameters
     ----------
-    fm (int)         : Number of Feature Maps at the first Convolution, width of the model.
-    n_convs (int)    : Number of blocks of convolutions (2 convolutions per block), depth of the model.
-    resampling (int) : Resampling Frequency.
-    kernel_size (int): Size of the convolutions kernels .
-    Guidelines on how to set those parameters are available in the appendix of the paper.
-    It is advised to read it before using the model. Default parameter should be effective for MI decoding.
+    fm: int
+        Number of Feature Maps at the first Convolution, width of the model.
+    n_convs: int
+        Number of blocks of convolutions (2 convolutions per block), depth of the model.
+    resampling: int
+        Resampling Frequency.
+    kernel_size: int
+        Size of the convolutions kernels.
 
     References
     ----------
-    .. [1] Yassine El Ouahidi, Vincent Gripon, Bastien Pasdeloup, Ghaith Bouallegue, Nicolas Farrugia, Giulia Lioi (2023).
-    A Strong and Simple Deep Learning Baseline for BCI Motor Imagery Decoding.
-    Arxiv preprint, under review for a journal publication.
-    Online: https://arxiv.org/abs/2309.07159
+    .. [Yassine2023] Yassine El Ouahidi, V. Gripon, B. Pasdeloup, G. Bouallegue
+    N. Farrugia, G. Lioi, 2023. A Strong and Simple Deep Learning Baseline for
+    BCI Motor Imagery Decoding. Arxiv preprint. arxiv.org/abs/2309.07159
+    .. [Yassine2023] Yassine El Ouahidi, V. Gripon, B. Pasdeloup, G. Bouallegue
+    N. Farrugia, G. Lioi, 2023. A Strong and Simple Deep Learning Baseline for
+    BCI Motor Imagery Decoding. GitHub repository.
+    https://github.com/elouayas/EEGSimpleConv.
+
     """
 
     def __init__(
         self,
+        # Base arguments
         n_outputs=None,
         n_chans=None,
         sfreq=None,
-        # Base arguments
+        # Model specific arguments
         fm=128,
         n_convs=2,
         resampling=80,
         kernel_size=8,
-        # Model specific arguments
+        # Other ways to initialize the model
         chs_info=None,
         n_times=None,
         input_window_seconds=None,
-        # Not used base arguments
     ):
         super().__init__(
             n_outputs=n_outputs,
@@ -100,7 +126,20 @@ class EEGSimpleConv(EEGModuleMixin, torch.nn.Module):
         self.blocks = torch.nn.ModuleList(self.blocks)
         self.final_layer = torch.nn.Linear(oldfm, n_outputs)
 
-    def forward(self, x):  # x = Batch x Channels x Time
+    def forward(self, x):
+        """
+        Forward pass of the model.
+
+        Parameters
+        ----------
+        x: PyTorch Tensor
+            Input tensor of shape (batch_size, n_channels, n_times)
+
+        Returns
+        -------
+        PyTorch Tensor
+            Output tensor of shape (batch_size, n_outputs)
+        """
         x_rs = self.rs(x.contiguous())
         y = torch.relu(self.bn(self.conv(x_rs)))
         for seq in self.blocks:
