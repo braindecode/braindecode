27aa45b376760af9bf705002e172b8d25e77c228
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2024-04-03 01:18:21 +0200

    adding functions tests

--- braindecode/models/functions.py
+++ braindecode/models/functions.py
@@ -10,7 +10,7 @@ def square(x):
 
 
 def safe_log(x, eps=1e-6):
-    """ Prevents :math:`log(0)` by using :math:`log(max(x, eps))`."""
+    """Prevents :math:`log(0)` by using :math:`log(max(x, eps))`."""
     return torch.log(torch.clamp(x, min=eps))
 
 
@@ -48,14 +48,40 @@ def transpose_time_to_spat(x):
 
 
 def _modify_eig_forward(X, function_applied):
-    """Modified eigenvalue of symmetric matrix X."""
+    """Modified eigenvalue of symmetric matrix X.
+
+    Steps:
+
+    - Computes the eigenvalue decomposition of a real symmetric matrix.
+    - Applies a function to the eigenvalues of the matrix.
+    - Reconstructs the matrix with the modified eigenvalues.
+
+    Parameters
+    ----------
+    X : torch.Tensor
+        Symmetric matrix of shape (n_channels, n_channels)
+    function_applied : callable function to be applied to the eigenvalues of X
+        Function to apply to the eigenvalues of X
+
+    Returns
+    -------
+    output : torch.Tensor
+        Modified eigenvalue of X
+    s : torch.Tensor
+        Eigenvalues of X
+    U : torch.Tensor
+        Eigenvectors of X
+    s_modified : torch.Tensor
+        Modified eigenvalues of X
+    """
     s, U = torch.linalg.eigh(X)
     s_modified = function_applied(s)
     output = U @ torch.diag_embed(s_modified) @ U.transpose(-1, -2)
     return output, s, U, s_modified
 
 
-def _modify_eig_backward(grad_output, s, U, s_modified, derivative, threshold=1e-4):
+def _modify_eig_backward(grad_output, s, U, s_modified, derivative,
+                         threshold=1e-4):
     """
     Backward pass of modified eigenvalue of symmetric matrix X.
 
@@ -64,26 +90,60 @@ def _modify_eig_backward(grad_output, s, U, s_modified, derivative, threshold=1e
     grad_output : torch.Tensor
         Gradient of the loss with respect to the output of the forward pass
     s: torch.Tensor
+        Eigenvalues of X
+    U: torch.Tensor
+        Eigenvectors of X
+    s_modified: torch.Tensor
+        Modified eigenvalues of X
+    derivative: callable function to be applied to the eigenvalues of X
+        Derivative of the function applied to the eigenvalues of X
+    threshold: float
+        Threshold to consider two eigenvalues as equal to each other
+
+    Returns
+    -------
+    grad_input : torch.Tensor
+        Gradient of the loss with respect to the input of the forward pass
 
     """
-
-    """Backward pass of modified eigenvalue of symmetric matrix X."""
     # compute Loewner matrix
-    denominator = s.unsqueeze(-1) - s.unsqueeze(-1).transpose(-1, -2)
+    # Compute the difference between each pair of eigenvalues,
+    # expanding dimensions
+    denominator = s.unsqueeze(-1) - s.unsqueeze(-2)
+
+    # Create a boolean mask where the absolute difference between
+    # eigenvalues is less than the threshold
     is_eq = denominator.abs() < threshold
+    # For pairs of eigenvalues that are considered equal,
+    # set the denominator to 1 to avoid division by zero
     denominator[is_eq] = 1.0
 
-    # case: sigma_i != sigma_j
-    numerator = s_modified.unsqueeze(-1) - s_modified.unsqueeze(-1).transpose(-1, -2)
+    # Compute the difference between each pair of modified eigenvalues,
+    # expanding dimensions
 
+    # case: sigma_i != sigma_j
+    numerator = s_modified.unsqueeze(-1) - s_modified.unsqueeze(-2)
+
+    # Compute the derivative of the function applied to the eigenvalues
     # case: sigma_i == sigma_j
     s_derivativated = derivative(s)
-    numerator[is_eq] = 0.5 * (
-        s_derivativated.unsqueeze(-1) + s_derivativated.unsqueeze(-1).transpose(-1, -2)
-    )[is_eq]
+    # For pairs of eigenvalues that are considered equal,
+    # set the numerator to the average of their derivatives
+
+    numerator[is_eq] = (
+        0.5
+        * (
+            s_derivativated.unsqueeze(-1)
+            + s_derivativated.unsqueeze(-2)
+        )[is_eq]
+    )
+    # Compute the Loewner matrix,
+    # which is the element-wise division of the numerator and the denominator
     L = numerator / denominator
 
-    grad_input = U @  (L * (U.transpose(-1, -2) @ grad_output @ U)) @ U.transpose(-1, -2)
+    # Compute the gradient of the loss input with
+    #  grad_input = U * (L * (U^T * G * U)) * U^T (I think)
+    grad_input = U @ (L * (U.transpose(-1, -2) @ grad_output @ U)) @ U.transpose(-1, -2)
 
     return grad_input
 
@@ -109,9 +169,12 @@ class logm(torch.autograd.Function):
     .. [1] Brooks et al. 2019, Riemannian batch normalization
             for SPD neural networks, NeurIPS
     """
+
     @staticmethod
     def forward(ctx, X):
-        def function_applied(s): return s.log()
+        def function_applied(s):
+            return s.log()
+
         output, s, U, s_modified = _modify_eig_forward(X, function_applied)
         ctx.save_for_backward(s, U, s_modified)
         return output
@@ -119,7 +182,10 @@ class logm(torch.autograd.Function):
     @staticmethod
     def backward(ctx, grad_output):
         s, U, s_modified = ctx.saved_tensors
-        def derivative(s): return s.reciprocal()
+
+        def derivative(s):
+            return s.reciprocal()
+
         return _modify_eig_backward(grad_output, s, U, s_modified, derivative)
 
 
@@ -146,9 +212,12 @@ class regm(torch.autograd.Function):
     .. [1] Brooks et al. 2019, Riemannian batch normalization
             for SPD neural networks, NeurIPS
     """
+
     @staticmethod
     def forward(ctx, X, threshold):
-        def function_applied(s): return s.clamp(min=threshold)
+        def function_applied(s):
+            return s.clamp(min=threshold)
+
         output, s, U, s_modified = _modify_eig_forward(X, function_applied)
         ctx.save_for_backward(s, U, s_modified)
         ctx.threshold_ = threshold
@@ -157,5 +226,8 @@ class regm(torch.autograd.Function):
     @staticmethod
     def backward(ctx, grad_output):
         s, U, s_modified = ctx.saved_tensors
-        def derivative(s): return s > ctx.threshold_
+
+        def derivative(s):
+            return s > ctx.threshold_
+
         return _modify_eig_backward(grad_output, s, U, s_modified, derivative), None
