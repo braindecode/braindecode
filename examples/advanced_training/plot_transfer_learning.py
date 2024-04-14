c083386ec5d0d3c1fc795ba6dd13e1b3026be048
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2024-04-14 20:34:01 +0200

    pre-commit

--- examples/advanced_training/plot_transfer_learning.py
+++ examples/advanced_training/plot_transfer_learning.py
@@ -29,25 +29,30 @@ n_jobs = 1
 # downstream classification performance.
 
 # %%
-TUAB_path = 'PATH_TO_TUAB_DATASET'
-NMT_path = 'PATH_TO_NMT_DATASET'
+TUAB_path = "PATH_TO_TUAB_DATASET"
+NMT_path = "PATH_TO_NMT_DATASET"
 
-from braindecode.datautil.serialization import  load_concat_dataset
+from braindecode.datautil.serialization import load_concat_dataset
 
-TUAB_ds = load_concat_dataset(TUAB_path, preload=False,
-                            target_name=['pathological','age','gender'] ,#)
-                            ids_to_load=range(20)
-                            )
+TUAB_ds = load_concat_dataset(
+    TUAB_path,
+    preload=False,
+    target_name=["pathological", "age", "gender"],  # )
+    ids_to_load=range(20),
+)
 
 # %%
 import pandas as pd
+
 print("target is being set to pathological clf")
-target = TUAB_ds.description['pathological'].astype(int)
+target = TUAB_ds.description["pathological"].astype(int)
 for d, y in zip(TUAB_ds.datasets, target):
-    d.description['pathological'] = y
-    d.target_name = 'pathological'
+    d.description["pathological"] = y
+    d.target_name = "pathological"
     d.target = d.description[d.target_name]
-TUAB_ds.set_description(pd.DataFrame([d.description for d in TUAB_ds.datasets]), overwrite=True)
+TUAB_ds.set_description(
+    pd.DataFrame([d.description for d in TUAB_ds.datasets]), overwrite=True
+)
 
 # %% [markdown]
 # ### Splitting dataset into train, valid and test sets
@@ -58,8 +63,8 @@ TUAB_ds.set_description(pd.DataFrame([d.description for d in TUAB_ds.datasets]),
 
 # %%
 # split based on train split from dataset
-train_set = TUAB_ds.split('train')['True']
-test_set = TUAB_ds.split('train')['False']
+train_set = TUAB_ds.split("train")["True"]
+test_set = TUAB_ds.split("train")["False"]
 
 # %% [markdown]
 # ## Creating the model
@@ -73,24 +78,26 @@ n_chans = 21
 n_classes = 2
 input_window_samples = 6000
 drop_prob = 0.5
-cuda = True # Set to False if you don't have a GPU
+cuda = True  # Set to False if you don't have a GPU
 n_start_chans = 25
 final_conv_length = 1
 n_chan_factor = 2
 stride_before_pool = True
 # input_window_samples =6000
 model = Deep4Net(
-            n_chans, n_classes,
-            n_filters_time=n_start_chans,
-            n_filters_spat=n_start_chans,
-            input_window_samples=input_window_samples,
-            n_filters_2=int(n_start_chans * n_chan_factor),
-            n_filters_3=int(n_start_chans * (n_chan_factor ** 2.0)),
-            n_filters_4=int(n_start_chans * (n_chan_factor ** 3.0)),
-            final_conv_length=final_conv_length,
-            stride_before_pool=stride_before_pool,
-            drop_prob=drop_prob)
-            # Send model to GPU
+    n_chans,
+    n_classes,
+    n_filters_time=n_start_chans,
+    n_filters_spat=n_start_chans,
+    input_window_samples=input_window_samples,
+    n_filters_2=int(n_start_chans * n_chan_factor),
+    n_filters_3=int(n_start_chans * (n_chan_factor**2.0)),
+    n_filters_4=int(n_start_chans * (n_chan_factor**3.0)),
+    final_conv_length=final_conv_length,
+    stride_before_pool=stride_before_pool,
+    drop_prob=drop_prob,
+)
+# Send model to GPU
 if cuda:
     model.cuda()
 
@@ -111,21 +118,26 @@ from IPython.utils import io
 from braindecode.datautil.windowers import create_fixed_length_windows
 
 with io.capture_output() as captured:
-        window_train_set = create_fixed_length_windows(train_set,
-                                                    start_offset_samples=0,
-                                                    stop_offset_samples=None,
-                                                    preload=True,
-                                                    window_size_samples=input_window_samples,
-                                                    window_stride_samples=n_preds_per_input,
-                                                    drop_last_window=True,)
+    window_train_set = create_fixed_length_windows(
+        train_set,
+        start_offset_samples=0,
+        stop_offset_samples=None,
+        preload=True,
+        window_size_samples=input_window_samples,
+        window_stride_samples=n_preds_per_input,
+        drop_last_window=True,
+    )
 
 with io.capture_output() as captured:
-        window_test_set = create_fixed_length_windows(test_set,
-                                                    start_offset_samples=0,
-                                                    stop_offset_samples=None,preload=False,
-                                                    window_size_samples=input_window_samples,
-                                                    window_stride_samples=n_preds_per_input,
-                                                    drop_last_window=False,)
+    window_test_set = create_fixed_length_windows(
+        test_set,
+        start_offset_samples=0,
+        stop_offset_samples=None,
+        preload=False,
+        window_size_samples=input_window_samples,
+        window_stride_samples=n_preds_per_input,
+        drop_last_window=False,
+    )
 
 # %% [markdown]
 # ## defining the classifier
@@ -144,26 +156,32 @@ n_epochs = 10
 batch_size = 64
 
 clf = EEGClassifier(
-                model,
-                cropped=True,
-                classes=[0, 1],
-                criterion=CroppedLoss,
-                criterion__loss_function=torch.nn.functional.nll_loss,
-                optimizer=torch.optim.AdamW,
-                train_split=predefined_split(window_test_set),
-                optimizer__lr=lr,
-                optimizer__weight_decay=weight_decay,
-                iterator_train__shuffle=True,
-                batch_size=batch_size,
-                callbacks=[
-                    "accuracy", "balanced_accuracy","f1",("lr_scheduler", LRScheduler('CosineAnnealingLR', T_max=n_epochs - 1)),
-                    ],
-                device='cuda' if cuda else 'cpu',
-                )
+    model,
+    cropped=True,
+    classes=[0, 1],
+    criterion=CroppedLoss,
+    criterion__loss_function=torch.nn.functional.nll_loss,
+    optimizer=torch.optim.AdamW,
+    train_split=predefined_split(window_test_set),
+    optimizer__lr=lr,
+    optimizer__weight_decay=weight_decay,
+    iterator_train__shuffle=True,
+    batch_size=batch_size,
+    callbacks=[
+        "accuracy",
+        "balanced_accuracy",
+        "f1",
+        ("lr_scheduler", LRScheduler("CosineAnnealingLR", T_max=n_epochs - 1)),
+    ],
+    device="cuda" if cuda else "cpu",
+)
 
-clf.initialize() # This is important!
-print('classifier initialized')
-print("Number of parameters = ", sum(p.numel() for p in model.parameters() if p.requires_grad))
+clf.initialize()  # This is important!
+print("classifier initialized")
+print(
+    "Number of parameters = ",
+    sum(p.numel() for p in model.parameters() if p.requires_grad),
+)
 
 
 # %% [markdown]
@@ -183,40 +201,50 @@ clf.fit(window_train_set, y=None, epochs=n_epochs)
 # ## saving the model
 
 # %%
-result_path = 'PATH_TO_SAVE_THE_MODEL'
+result_path = "PATH_TO_SAVE_THE_MODEL"
 path = result_path + "model_{}.pt".format(random_state)
 torch.save(clf.module, path)
 path = result_path + "state_dict_{}.pt".format(random_state)
 torch.save(clf.module.state_dict(), path)
 
-clf.save_params(f_params=result_path +'model.pkl', f_optimizer= result_path +'opt.pkl', f_history=result_path +'history.json')
+clf.save_params(
+    f_params=result_path + "model.pkl",
+    f_optimizer=result_path + "opt.pkl",
+    f_history=result_path + "history.json",
+)
 
 # %% [markdown]
 # ## Load NMT dataset
 
 # %%
 import mne
-mne.set_log_level('ERROR')
 
-NMT_ds = load_concat_dataset(NMT_path, preload=False,
-                            target_name=['pathological','age','gender'] ,#)
-                            ids_to_load=range(200)
-                            )
+mne.set_log_level("ERROR")
+
+NMT_ds = load_concat_dataset(
+    NMT_path,
+    preload=False,
+    target_name=["pathological", "age", "gender"],  # )
+    ids_to_load=range(200),
+)
 
 # %%
 # split based on train split from dataset
-train_set = NMT_ds.split('train')['True']
-test_set = NMT_ds.split('train')['False']
+train_set = NMT_ds.split("train")["True"]
+test_set = NMT_ds.split("train")["False"]
 
 # %%
 import pandas as pd
+
 print("target is being set to pathological clf")
-target = NMT_ds.description['pathological'].astype(int)
+target = NMT_ds.description["pathological"].astype(int)
 for d, y in zip(NMT_ds.datasets, target):
-    d.description['pathological'] = y
-    d.target_name = 'pathological'
+    d.description["pathological"] = y
+    d.target_name = "pathological"
     d.target = d.description[d.target_name]
-NMT_ds.set_description(pd.DataFrame([d.description for d in NMT_ds.datasets]), overwrite=True)
+NMT_ds.set_description(
+    pd.DataFrame([d.description for d in NMT_ds.datasets]), overwrite=True
+)
 
 # %% [markdown]
 # ### extracting windows
@@ -226,38 +254,43 @@ from IPython.utils import io
 from braindecode.datautil.windowers import create_fixed_length_windows
 
 with io.capture_output() as captured:
-        window_train_set = create_fixed_length_windows(train_set,
-                                                    start_offset_samples=0,
-                                                    stop_offset_samples=None,
-                                                    preload=True,
-                                                    window_size_samples=input_window_samples,
-                                                    window_stride_samples=n_preds_per_input,
-                                                    drop_last_window=True,)
+    window_train_set = create_fixed_length_windows(
+        train_set,
+        start_offset_samples=0,
+        stop_offset_samples=None,
+        preload=True,
+        window_size_samples=input_window_samples,
+        window_stride_samples=n_preds_per_input,
+        drop_last_window=True,
+    )
 with io.capture_output() as captured:
-        window_test_set = create_fixed_length_windows(test_set,
-                                                    start_offset_samples=0,
-                                                    stop_offset_samples=None,preload=False,
-                                                    window_size_samples=input_window_samples,
-                                                    window_stride_samples=n_preds_per_input,
-                                                    drop_last_window=False,)
+    window_test_set = create_fixed_length_windows(
+        test_set,
+        start_offset_samples=0,
+        stop_offset_samples=None,
+        preload=False,
+        window_size_samples=input_window_samples,
+        window_stride_samples=n_preds_per_input,
+        drop_last_window=False,
+    )
 
 # %% [markdown]
 # ## loading the pre trained model
 
 # %%
-load_path = result_path + 'state_dict_2024.pt'
+load_path = result_path + "state_dict_2024.pt"
 state_dicts = torch.load(load_path)
-model.load_state_dict(state_dicts, strict= False)
-print('pre-trained model loaded using pytorch')
+model.load_state_dict(state_dicts, strict=False)
+print("pre-trained model loaded using pytorch")
 
 ## freeze layers ##
 freeze = False
 if freeze:
     for ii, (name, param) in enumerate(model.named_parameters()):
         # if 'temporal_block_0' in name or 'temporal_block_1' in name or 'temporal_block_2' in name or 'temporal_block_3' in name: # or 'temporal_block_5' in name or 'conv_classifier' in name:
-        if 'conv_classifier' not in name:
+        if "conv_classifier" not in name:
             param.requires_grad = False
-            print('param:', name, param.requires_grad)
+            print("param:", name, param.requires_grad)
 
 # %% [markdown]
 # ## fine tuning the model on NMT dataset
@@ -275,26 +308,32 @@ n_epochs = 10
 batch_size = 64
 
 clf = EEGClassifier(
-                model,
-                cropped=True,
-                classes=[0, 1],
-                criterion=CroppedLoss,
-                criterion__loss_function=torch.nn.functional.nll_loss,
-                optimizer=torch.optim.AdamW,
-                train_split=predefined_split(window_test_set),
-                optimizer__lr=lr,
-                optimizer__weight_decay=weight_decay,
-                iterator_train__shuffle=True,
-                batch_size=batch_size,
-                callbacks=[
-                    "accuracy", "balanced_accuracy","f1",("lr_scheduler", LRScheduler('CosineAnnealingLR', T_max=n_epochs - 1)),
-                    ],
-                device='cuda' if cuda else 'cpu',
-                )
+    model,
+    cropped=True,
+    classes=[0, 1],
+    criterion=CroppedLoss,
+    criterion__loss_function=torch.nn.functional.nll_loss,
+    optimizer=torch.optim.AdamW,
+    train_split=predefined_split(window_test_set),
+    optimizer__lr=lr,
+    optimizer__weight_decay=weight_decay,
+    iterator_train__shuffle=True,
+    batch_size=batch_size,
+    callbacks=[
+        "accuracy",
+        "balanced_accuracy",
+        "f1",
+        ("lr_scheduler", LRScheduler("CosineAnnealingLR", T_max=n_epochs - 1)),
+    ],
+    device="cuda" if cuda else "cpu",
+)
 
-clf.initialize() # This is important!
-print('classifier initialized')
-print("Number of parameters = ", sum(p.numel() for p in model.parameters() if p.requires_grad))
+clf.initialize()  # This is important!
+print("classifier initialized")
+print(
+    "Number of parameters = ",
+    sum(p.numel() for p in model.parameters() if p.requires_grad),
+)
 
 
 # %%
