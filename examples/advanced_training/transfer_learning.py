4211c70ae01ce3113d13c1d7770e9c1c2a4a0ce6
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2024-04-14 20:44:48 +0200

    renaming and fixing

--- examples/advanced_training/transfer_learning.py
+++ examples/advanced_training/transfer_learning.py
@@ -1,24 +1,53 @@
-# %% [markdown]
+"""
+How to process and do transfer learning with braindecode
+====================================================================================
+
+This tutorial shows how-to perform transfer learning using braindecode.
+Indeed, it is known that the best augmentation to use often dependent on the task
+or phenomenon studied. Here we follow the methodology proposed in [1]_ on the
+openly available BCI IV 2a Dataset and another dataset.
+
+
+.. topic:: Transfer Learning
+
+    REPLACE... Data augmentation could be a step in training deep learning models.
+    For decoding brain signals, recent studies have shown that artificially
+    generating samples may increase the final performance of a deep learning model [1]_.
+    Other studies have shown that data augmentation can be used to cast
+    a self-supervised paradigm, presenting a more diverse
+    view of the data, both with pretext tasks and contrastive learning [2]_.
+
+
+REPLACE...
+
+Figure about transfer learning.
+
+
+.. contents:: This example covers:
+   :local:
+   :depth: 2
+
+"""
+# Authors: MJ Bayazi <mj.darvishi92@gmail.com>
+#           Bruno Aristimunha <a.bruno@ufabc.edu.br>
+# License: BSD (3-clause)
+
+######################################################################
+# Loading and preprocessing the dataset
+# -------------------------------------
 #
-# # Transfer learning for EEG
+# Loading
+# ~~~~~~~
 #
 # This example shows how to train a neural network with supervision on TUH [2]
 # EEG data and transfer the model to NMT [3] EEG dataset. We follow the approach of [1]
-#
-
-# %%
-# Authors: MJ Bayazi <mj.darvishi92@gmail.com>
-#
-# License: BSD (3-clause)
-
 
 random_state = 2024
 n_jobs = 1
 
-# %% [markdown]
 # ## Loading and preprocessing the dataset
 #
-# ### Load  and save the raw recordings
+# ### Load and save the raw recordings
 #
 # Here we assume you already load and preprocess the raw recordings for both TUAB and NMT datasetsand saved the file in `TUAB_path' and 'NMT_path' respectively. To read more see this notebook [here](https://braindecode.org/stable/auto_examples/applied_examples/plot_tuh_eeg_corpus.html)
 #
@@ -28,7 +57,7 @@ n_jobs = 1
 # this example with more recordings should yield better representations and
 # downstream classification performance.
 
-# %%
+
 TUAB_path = "PATH_TO_TUAB_DATASET"
 NMT_path = "PATH_TO_NMT_DATASET"
 
@@ -41,10 +70,11 @@ TUAB_ds = load_concat_dataset(
     ids_to_load=range(20),
 )
 
-# %%
+######################################################################
+# Target is being set to pathological
+# -------------------------------------
 import pandas as pd
 
-print("target is being set to pathological clf")
 target = TUAB_ds.description["pathological"].astype(int)
 for d, y in zip(TUAB_ds.datasets, target):
     d.description["pathological"] = y
@@ -54,24 +84,24 @@ TUAB_ds.set_description(
     pd.DataFrame([d.description for d in TUAB_ds.datasets]), overwrite=True
 )
 
-# %% [markdown]
-# ### Splitting dataset into train, valid and test sets
+# Splitting dataset into train, valid and test sets
 #
 # We split the recordings by subject into train, validation and
 # testing sets.
 #
 
-# %%
 # split based on train split from dataset
 train_set = TUAB_ds.split("train")["True"]
 test_set = TUAB_ds.split("train")["False"]
 
-# %% [markdown]
-# ## Creating the model
-#
-# We can now create the deep learning model. In this tutorial, we use DeepNet introduced in [4].
 
-# %%
+######################################################################
+# Create the models
+# -------------------------------------
+# We can now create the deep learning model.
+# In this tutorial, we use DeepNet introduced in [4].
+
+
 from braindecode.models import Deep4Net
 
 n_chans = 21
@@ -107,43 +137,40 @@ to_dense_prediction_model(model)
 
 n_preds_per_input = get_output_shape(model, n_chans, input_window_samples)[2]
 
-# %% [markdown]
-# ### Extracting windows
+######################################################################
+# Extracting windows
+# -------------------------------------
 #
 # We extract 60-s windows to be used in both datasets. We use a window size of 6000 samples.
 #
 
-# %%
-from IPython.utils import io
 from braindecode.datautil.windowers import create_fixed_length_windows
 
-with io.capture_output() as captured:
-    window_train_set = create_fixed_length_windows(
-        train_set,
-        start_offset_samples=0,
-        stop_offset_samples=None,
-        preload=True,
-        window_size_samples=input_window_samples,
-        window_stride_samples=n_preds_per_input,
-        drop_last_window=True,
-    )
+window_train_set = create_fixed_length_windows(
+    train_set,
+    start_offset_samples=0,
+    stop_offset_samples=None,
+    preload=True,
+    window_size_samples=input_window_samples,
+    window_stride_samples=n_preds_per_input,
+    drop_last_window=True,
+)
 
-with io.capture_output() as captured:
-    window_test_set = create_fixed_length_windows(
-        test_set,
-        start_offset_samples=0,
-        stop_offset_samples=None,
-        preload=False,
-        window_size_samples=input_window_samples,
-        window_stride_samples=n_preds_per_input,
-        drop_last_window=False,
-    )
+window_test_set = create_fixed_length_windows(
+    test_set,
+    start_offset_samples=0,
+    stop_offset_samples=None,
+    preload=False,
+    window_size_samples=input_window_samples,
+    window_stride_samples=n_preds_per_input,
+    drop_last_window=False,
+)
 
-# %% [markdown]
-# ## defining the classifier
-#
+######################################################################
+# Defining the classifier windows
+# -------------------------------------
 
-# %%
+
 from braindecode import EEGClassifier
 from braindecode.training.losses import CroppedLoss
 import torch
@@ -184,9 +211,10 @@ print(
 )
 
 
-# %% [markdown]
-# ## Training
-#
+######################################################################
+# Training
+# -------------------------------------
+
 # We can now train our network on the TUAB. We use similar
 # hyperparameters as in [1]_, but reduce the number of epochs and
 # increase the learning rate to account for the smaller setting of
@@ -194,13 +222,15 @@ print(
 #
 #
 
-# %%
+
 clf.fit(window_train_set, y=None, epochs=n_epochs)
 
-# %% [markdown]
-# ## saving the model
 
-# %%
+######################################################################
+# Saving the model
+# -------------------------------------
+
+
 result_path = "PATH_TO_SAVE_THE_MODEL"
 path = result_path + "model_{}.pt".format(random_state)
 torch.save(clf.module, path)
@@ -213,10 +243,12 @@ clf.save_params(
     f_history=result_path + "history.json",
 )
 
-# %% [markdown]
-# ## Load NMT dataset
 
-# %%
+######################################################################
+# Loading the NMT dataset
+# -------------------------------------
+
+
 import mne
 
 mne.set_log_level("ERROR")
@@ -228,12 +260,11 @@ NMT_ds = load_concat_dataset(
     ids_to_load=range(200),
 )
 
-# %%
+######################################################################
 # split based on train split from dataset
 train_set = NMT_ds.split("train")["True"]
 test_set = NMT_ds.split("train")["False"]
 
-# %%
 import pandas as pd
 
 print("target is being set to pathological clf")
@@ -246,38 +277,33 @@ NMT_ds.set_description(
     pd.DataFrame([d.description for d in NMT_ds.datasets]), overwrite=True
 )
 
-# %% [markdown]
 # ### extracting windows
 
-# %%
-from IPython.utils import io
 from braindecode.datautil.windowers import create_fixed_length_windows
 
-with io.capture_output() as captured:
-    window_train_set = create_fixed_length_windows(
-        train_set,
-        start_offset_samples=0,
-        stop_offset_samples=None,
-        preload=True,
-        window_size_samples=input_window_samples,
-        window_stride_samples=n_preds_per_input,
-        drop_last_window=True,
-    )
-with io.capture_output() as captured:
-    window_test_set = create_fixed_length_windows(
-        test_set,
-        start_offset_samples=0,
-        stop_offset_samples=None,
-        preload=False,
-        window_size_samples=input_window_samples,
-        window_stride_samples=n_preds_per_input,
-        drop_last_window=False,
-    )
+window_train_set = create_fixed_length_windows(
+    train_set,
+    start_offset_samples=0,
+    stop_offset_samples=None,
+    preload=True,
+    window_size_samples=input_window_samples,
+    window_stride_samples=n_preds_per_input,
+    drop_last_window=True,
+)
+window_test_set = create_fixed_length_windows(
+    test_set,
+    start_offset_samples=0,
+    stop_offset_samples=None,
+    preload=False,
+    window_size_samples=input_window_samples,
+    window_stride_samples=n_preds_per_input,
+    drop_last_window=False,
+)
+######################################################################
+# Loading the pre-trained model
+# -------------------------------------
 
-# %% [markdown]
-# ## loading the pre trained model
 
-# %%
 load_path = result_path + "state_dict_2024.pt"
 state_dicts = torch.load(load_path)
 model.load_state_dict(state_dicts, strict=False)
@@ -287,15 +313,17 @@ print("pre-trained model loaded using pytorch")
 freeze = False
 if freeze:
     for ii, (name, param) in enumerate(model.named_parameters()):
-        # if 'temporal_block_0' in name or 'temporal_block_1' in name or 'temporal_block_2' in name or 'temporal_block_3' in name: # or 'temporal_block_5' in name or 'conv_classifier' in name:
+        # if 'temporal_block_0' in name or 'temporal_block_1'
+        # in name or 'temporal_block_2' in name or
+        # 'temporal_block_3' in name: # or 'temporal_block_5'
+        # in name or 'conv_classifier' in name:
         if "conv_classifier" not in name:
             param.requires_grad = False
             print("param:", name, param.requires_grad)
 
-# %% [markdown]
 # ## fine tuning the model on NMT dataset
 
-# %%
+
 from braindecode import EEGClassifier
 from braindecode.training.losses import CroppedLoss
 import torch
@@ -326,6 +354,7 @@ clf = EEGClassifier(
         ("lr_scheduler", LRScheduler("CosineAnnealingLR", T_max=n_epochs - 1)),
     ],
     device="cuda" if cuda else "cpu",
+    max_epochs=n_epochs,
 )
 
 clf.initialize()  # This is important!
@@ -336,21 +365,35 @@ print(
 )
 
 
-# %%
-clf.fit(window_train_set, y=None, epochs=n_epochs)
+######################################################################
+clf.fit(window_train_set, y=None)
 
-# %% [markdown]
-# ## Conclusion
+######################################################################
+# Conclusion
+# -------------------------------------
 #
 # In this example, we used transfer learning (TL) as a way to learn
 # representations from a large EEG data and transfer to a smaller dataset.
+# You can put one of the results from your paper here.
+
+
+######################################################################
+# References
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 #
-# ## References
-#
-# .. [1] Darvishi-Bayazi, M. J., Ghaemi, M. S., Lesort, T., Arefin, M. R., Faubert, J., & Rish, I. (2024). Amplifying pathological detection in EEG signaling pathways through cross-dataset transfer learning. Computers in Biology and Medicine, 169, 107893.
-#
-# .. [2] Shawki, N., Shadin, M. G., Elseify, T., Jakielaszek, L., Farkas, T., Persidsky, Y., ... & Picone, J. (2022). Correction to: The temple university hospital digital pathology corpus. In Signal Processing in Medicine and Biology: Emerging Trends in Research and Applications (pp. C1-C1). Cham: Springer International Publishing..
-#
-# .. [3] Khan, H. A., Ul Ain, R., Kamboh, A. M., & Butt, H. T. (2022). The NMT scalp EEG dataset: an open-source annotated dataset of healthy and pathological EEG recordings for predictive modeling. Frontiers in neuroscience, 15, 755817.
-#
+# .. [1] Darvishi-Bayazi, M. J., Ghaemi, M. S., Lesort, T., Arefin, M. R.,
+#    Faubert, J., & Rish, I. (2024). Amplifying pathological detection in
+#    EEG signaling pathways through cross-dataset transfer learning.
+#    Computers in Biology and Medicine, 169, 107893.
+# .. [2] Shawki, N., Shadin, M. G., Elseify, T., Jakielaszek, L., Farkas, T.,
+#    Persidsky, Y., ... & Picone, J. (2022). Correction to:
+#    The temple university hospital digital pathology corpus.
+#    In Signal Processing in Medicine and Biology:
+#    Emerging Trends in Research and Applications (pp. C1-C1).
+#    Cham: Springer International Publishing.
+# .. [3] Khan, H. A., Ul Ain, R., Kamboh, A. M., & Butt, H. T. (2022).
+#    The NMT scalp EEG dataset: an open-source annotated dataset of
+#    healthy and pathological EEG recordings for predictive modeling.
+#    Frontiers in neuroscience, 15, 755817.
+# .. [4] Aristimunha, B. ...
