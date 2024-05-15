a385af20367efee946bf1d8ee3db31a76bad5698
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2024-05-15 22:02:01 +0200

    Adding sparcnet

--- braindecode/models/__init__.py
+++ braindecode/models/__init__.py
@@ -28,6 +28,7 @@ from .modules import TimeDistributed
 from .util import _init_models_dict, models_mandatory_parameters
 from .labram import Labram
 from .eegsimpleconv import EEGSimpleConv
+from .sparcnet import SPARCNet
 
 # Call this last in order to make sure the dataset list is populated with
 # the models imported in this file.
