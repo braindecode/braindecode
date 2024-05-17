e085920712a346a72a4bd2dd75a138fd65abd7c7
John Muradeli <john.muradeli@gmail.com>
2024-05-17 17:44:27 +0400

    Remove redundant `eval()` & `train()` in `SleepStagerChambon2018`
    They don't affect shapes.
    
    This doesn't seem to belong anywhere in `whats_new`, nor is it important, so I omitted, but feel free to edit.

--- braindecode/models/sleep_stager_chambon_2018.py
+++ braindecode/models/sleep_stager_chambon_2018.py
@@ -133,10 +133,8 @@ class SleepStagerChambon2018(EEGModuleMixin, nn.Module):
             )
 
     def _len_last_layer(self, n_channels, input_size):
-        self.feature_extractor.eval()
         with torch.no_grad():
             out = self.feature_extractor(torch.Tensor(1, 1, n_channels, input_size))
-        self.feature_extractor.train()
         return len(out.flatten())
 
     def forward(self, x):
