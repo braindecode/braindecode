8a685a98f2f530f5050adafbdc3797888d5d5ce3
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2024-09-26 22:40:57 +0200

    DOC: copywriting

--- braindecode/models/eegdeformer.py
+++ braindecode/models/eegdeformer.py
@@ -1,3 +1,23 @@
+"""
+CBCR License 1.0
+
+Copyright 2022 Centre for Brain Computing Research (CBCR)
+
+Redistribution and use for non-commercial purpose in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials
+provided with the distribution.
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior
+written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTER-
+RUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+# Authors: Yi Ding
+#          Bruno Aristimunha <b.aristimunha@gmail.com> (braindecode adaptation)
+
 import torch
 from torch import nn, Tensor
 
@@ -266,3 +286,11 @@ class EEGDeformer(EEGModuleMixin, nn.Module):
         x = x + self.pos_embedding
         x = self.transformer(x)
         return self.final_layer(x)
+
+
+if __name__ == "__main__":
+    data = torch.ones((16, 32, 1000))
+    model = EEGDeformer(n_chans=32, n_times=1000, n_outputs=2)
+    print(model)
+
+    out = model(data)
