3ad94664f478b9eb9ed9686b1070c8a0205021d9
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2024-10-11 15:51:35 +0200

    EHN: no drop_prob in the model

--- test/unit_tests/models/test_integration.py
+++ test/unit_tests/models/test_integration.py
@@ -16,7 +16,7 @@ from skorch.dataset import ValidSplit
 
 from braindecode.models.util import models_dict, models_mandatory_parameters
 from braindecode import EEGClassifier
-from braindecode.models import SyncNet, EEGSimpleConv, EEGResNet, USleep, FBCNet
+from braindecode.models import SyncNet, EEGSimpleConv, EEGResNet, USleep, FBCNet, FBLightConvNet
 
 
 # Generating the channel info
@@ -322,7 +322,7 @@ def test_model_has_drop_prob_parameter(model_class):
     named 'drop_prob' or any parameter that starts with 'activation'.
     """
 
-    if model_class in [SyncNet, EEGSimpleConv, EEGResNet, USleep, FBCNet]:
+    if model_class in [SyncNet, EEGSimpleConv, EEGResNet, USleep, FBCNet, FBLightConvNet]:
         pytest.skip(
             f"Skipping {model_class} as not dropout layer")
 
