dc64cd5063e4e4af8b7445cf3b889280732b391e
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2024-10-14 21:00:38 +0200

    [EHN] fixing last test

--- test/acceptance_tests/test_model_architectures.py
+++ test/acceptance_tests/test_model_architectures.py
@@ -13,13 +13,12 @@ from braindecode.util import set_random_seeds
 def test_tcn():
     set_random_seeds(0, False)
     tcn = TCN(
-        n_in_chans=21,
+        n_chans=21,
         n_outputs=2,
         n_filters=55,
         n_blocks=5,
         kernel_size=16,
         drop_prob=0.05270154233150525,
-        add_log_softmax=True,
     )
     # braindecode models are always in eval mode after initialization
     # original model implementation was not
