475c8dded351d37a073c79871ee2c5a6bf061c4f
Pierre Guetschel <pierre.guetschel@donders.ru.nl>
2025-02-28 14:29:59 +0100

    Add BIDSDataset

--- braindecode/datasets/__init__.py
+++ braindecode/datasets/__init__.py
@@ -3,6 +3,7 @@ Loader code for some datasets.
 """
 
 from .base import WindowsDataset, BaseDataset, BaseConcatDataset
+from .bids import BIDSDataset
 from .moabb import MOABBDataset, HGD, BNCI2014001
 from .mne import create_from_mne_raw, create_from_mne_epochs
 from .tuh import TUH, TUHAbnormal
@@ -16,6 +17,7 @@ __all__ = [
     "WindowsDataset",
     "BaseDataset",
     "BaseConcatDataset",
+    "BIDSDataset",
     "MOABBDataset",
     "HGD",
     "BNCI2014001",
