d8e6db6b1acc715fc786918a45b56f97a46e001e
bruAristimunha <b.aristimunha@gmail.com>
2025-04-20 12:39:43 +0200

    finishing the tsinception

--- braindecode/models/tsinception.py
+++ braindecode/models/tsinception.py
@@ -20,7 +20,7 @@ class TSceptionV1(EEGModuleMixin, nn.Module):
 
     .. figure:: https://user-images.githubusercontent.com/58539144/74716976-80415e00-526a-11ea-9433-02ab2b753f6b.PNG
         :align: center
-        :alt: TIDNet Architecture
+        :alt: TSceptionV1 Architecture
 
     The model consists of temporal and spatial convolutional layers
     (Tception and Sception) designed to learn temporal and spatial features
@@ -114,31 +114,36 @@ class TSceptionV1(EEGModuleMixin, nn.Module):
                 for window in self.inception_windows
             ]
         )
+        self.batch_temporal_lay = nn.BatchNorm2d(self.number_filter_temp)
 
         # Define spatial convolutional layers (Sception)
+
+        pool_size_spat = self.pool_size // 4
+
         self.spatial_block_1 = self._conv_block(
             in_channels=self.number_filter_temp,
             out_channels=self.number_filter_spat,
             kernel_size=(self.n_chans, 1),
             stride=1,
-            pool_size=self.pool_size // 4,
-            activation=self.activation,
-        )
-        self.spatial_block_2 = self._conv_block(
-            in_channels=self.number_filter_temp,
-            out_channels=self.number_filter_spat,
-            kernel_size=(max(1, self.n_chans // 2), 1),
-            stride=(max(1, self.n_chans // 2), 1),
-            pool_size=self.pool_size // 4,
+            pool_size=pool_size_spat,
             activation=self.activation,
         )
 
-        self.batch_temporal_lay = nn.BatchNorm2d(self.number_filter_temp)
+        kernel_size_spat_2 = (max(1, self.n_chans // 2), 1)
+
+        self.spatial_block_2 = self._conv_block(
+            in_channels=self.number_filter_temp,
+            out_channels=self.number_filter_spat,
+            kernel_size=kernel_size_spat_2,
+            stride=kernel_size_spat_2,
+            pool_size=pool_size_spat,
+            activation=self.activation,
+        )
         self.batch_spatial_lay = nn.BatchNorm2d(self.number_filter_spat)
 
         # Calculate the size of the features after convolution and pooling layers
         self.feature_size = self._calculate_feature_size()
-
+        # self.feature_size = self.number_filter_spat *
         # Define the final classification layers
 
         self.dense_layer = nn.Sequential(
@@ -148,6 +153,67 @@ class TSceptionV1(EEGModuleMixin, nn.Module):
         )
 
         self.final_layer = nn.Linear(hidden_size, self.n_outputs)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass of the TSception model.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            Input tensor of shape (batch_size, n_channels, n_times).
+
+        Returns
+        -------
+        torch.Tensor
+            Output tensor of shape (batch_size, n_classes).
+        """
+        # Temporal Convolution
+        # shape: (batch_size, n_channels, n_times)
+        x = self.ensuredim(x)
+        # shape: (batch_size, 1, n_channels, n_times)
+
+        t_features = [layer(x) for layer in self.temporal_blocks]
+        # shape: (batch_size, number_filter_temp, n_channels,
+        #
+        t_out = torch.cat(t_features, dim=-1)
+
+        t_out = self.batch_temporal_lay(t_out)
+
+        # Spatial Convolution
+        s_out1 = self.spatial_block_1(t_out)
+        s_out2 = self.spatial_block_2(t_out)
+        s_out = torch.cat((s_out1, s_out2), dim=2)
+        s_out = self.batch_spatial_lay(s_out)
+
+        # Flatten and apply final layers
+        s_out = s_out.view(s_out.size(0), -1)
+        output = self.dense_layer(s_out)
+        output = self.final_layer(output)
+        return output
+
+    def _calculate_feature_size(self) -> int:
+        """
+        Calculates the size of the features after convolution and pooling layers.
+
+        Returns
+        -------
+        int
+            Flattened size of the features after convolution and pooling layers.
+        """
+        with torch.no_grad():
+            dummy_input = torch.ones(1, 1, self.n_chans, self.n_times)
+            t_features = [layer(dummy_input) for layer in self.temporal_blocks]
+            t_out = torch.cat(t_features, dim=-1)
+            t_out = self.batch_temporal_lay(t_out)
+
+            s_out1 = self.spatial_block_1(t_out)
+            s_out2 = self.spatial_block_2(t_out)
+            s_out = torch.cat((s_out1, s_out2), dim=2)
+            s_out = self.batch_spatial_lay(s_out)
+
+            feature_size = s_out.view(1, -1).size(1)
+        return feature_size
 
     @staticmethod
     def _conv_block(
@@ -192,58 +258,3 @@ class TSceptionV1(EEGModuleMixin, nn.Module):
             activation(),
             nn.AvgPool2d(kernel_size=(1, pool_size), stride=(1, pool_size)),
         )
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """
-        Forward pass of the TSception model.
-
-        Parameters
-        ----------
-        x : torch.Tensor
-            Input tensor of shape (batch_size, n_channels, n_times).
-
-        Returns
-        -------
-        torch.Tensor
-            Output tensor of shape (batch_size, n_classes).
-        """
-        # Temporal Convolution
-        x = self.ensuredim(x)
-        t_features = [layer(x) for layer in self.temporal_blocks]
-        t_out = torch.cat(t_features, dim=-1)
-        t_out = self.batch_temporal_lay(t_out)
-
-        # Spatial Convolution
-        s_out1 = self.spatial_block_1(t_out)
-        s_out2 = self.spatial_block_2(t_out)
-        s_out = torch.cat((s_out1, s_out2), dim=2)
-        s_out = self.batch_spatial_lay(s_out)
-
-        # Flatten and apply final layers
-        s_out = s_out.view(s_out.size(0), -1)
-        output = self.dense_layer(s_out)
-        output = self.final_layer(output)
-        return output
-
-    def _calculate_feature_size(self) -> int:
-        """
-        Calculates the size of the features after convolution and pooling layers.
-
-        Returns
-        -------
-        int
-            Flattened size of the features after convolution and pooling layers.
-        """
-        with torch.no_grad():
-            dummy_input = torch.ones(1, 1, self.n_chans, self.n_times)
-            t_features = [layer(dummy_input) for layer in self.temporal_blocks]
-            t_out = torch.cat(t_features, dim=-1)
-            t_out = self.batch_temporal_lay(t_out)
-
-            s_out1 = self.spatial_block_1(t_out)
-            s_out2 = self.spatial_block_2(t_out)
-            s_out = torch.cat((s_out1, s_out2), dim=2)
-            s_out = self.batch_spatial_lay(s_out)
-
-            feature_size = s_out.view(1, -1).size(1)
-        return feature_size
