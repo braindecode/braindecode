86e69e98daf968d1c2ed2d6a3e2393defc1034e7
bruAristimunha <b.aristimunha@gmail.com>
2025-04-20 13:44:23 +0200

    updating the init file

--- braindecode/models/__init__.py
+++ braindecode/models/__init__.py
@@ -4,7 +4,6 @@ Some predefined network architectures for EEG decoding.
 
 from .attentionbasenet import AttentionBaseNet
 from .base import EEGModuleMixin
-
 from .biot import BIOT
 from .eegconformer import EEGConformer
 from .eegitnet import EEGITNet
