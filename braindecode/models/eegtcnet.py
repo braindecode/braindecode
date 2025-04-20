ff2025f28858e0ae4844efc17a94968452feeb32
bruAristimunha <b.aristimunha@gmail.com>
2025-04-20 12:46:18 +0200

    changing the order EEGTCNet

--- braindecode/models/eegtcnet.py
+++ braindecode/models/eegtcnet.py
@@ -12,6 +12,148 @@ from braindecode.models.base import EEGModuleMixin
 from braindecode.models.modules import MaxNormLinear, Chomp1d
 
 
+class EEGTCNet(EEGModuleMixin, nn.Module):
+    """EEGTCNet model from Ingolfsson et al. (2020) [ingolfsson2020]_.
+
+    .. figure:: https://braindecode.org/dev/_static/model/eegtcnet.jpg
+        :align: center
+        :alt: EEGTCNet Architecture
+
+    Combining EEGNet and TCN blocks.
+
+    Parameters
+    ----------
+    activation : nn.Module, optional
+        Activation function to use. Default is `nn.ELU()`.
+    depth_multiplier : int, optional
+        Depth multiplier for the depthwise convolution. Default is 2.
+    filter_1 : int, optional
+        Number of temporal filters in the first convolutional layer. Default is 8.
+    kern_length : int, optional
+        Length of the temporal kernel in the first convolutional layer. Default is 64.
+    dropout : float, optional
+        Dropout rate. Default is 0.5.
+    depth : int, optional
+        Number of residual blocks in the TCN. Default is 2.
+    kernel_size : int, optional
+        Size of the temporal convolutional kernel in the TCN. Default is 4.
+    filters : int, optional
+        Number of filters in the TCN convolutional layers. Default is 12.
+    max_norm_const : float
+        Maximum L2-norm constraint imposed on weights of the last
+        fully-connected layer. Defaults to 0.25.
+
+    References
+    ----------
+    .. [ingolfsson2020] Ingolfsson, T. M., Hersche, M., Wang, X., Kobayashi, N.,
+        Cavigelli, L., & Benini, L. (2020). EEG-TCNet: An accurate temporal
+        convolutional network for embedded motor-imagery brain–machine interfaces.
+        https://doi.org/10.48550/arXiv.2006.00622
+    """
+
+    def __init__(
+        self,
+        # Signal related parameters
+        n_chans=None,
+        n_outputs=None,
+        n_times=None,
+        chs_info=None,
+        input_window_seconds=None,
+        sfreq=None,
+        # Model parameters
+        activation: nn.Module = nn.ELU,
+        depth_multiplier: int = 2,
+        filter_1: int = 8,
+        kern_length: int = 64,
+        drop_prob: float = 0.5,
+        depth: int = 2,
+        kernel_size: int = 4,
+        filters: int = 12,
+        max_norm_const: float = 0.25,
+    ):
+        super().__init__(
+            n_outputs=n_outputs,
+            n_chans=n_chans,
+            chs_info=chs_info,
+            n_times=n_times,
+            input_window_seconds=input_window_seconds,
+            sfreq=sfreq,
+        )
+        del n_outputs, n_chans, chs_info, n_times, input_window_seconds, sfreq
+
+        self.activation = activation
+        self.drop_prob = drop_prob
+        self.depth_multiplier = depth_multiplier
+        self.filter_1 = filter_1
+        self.kern_length = kern_length
+        self.depth = depth
+        self.kernel_size = kernel_size
+        self.filters = filters
+        self.max_norm_const = max_norm_const
+        self.filter_2 = self.filter_1 * self.depth_multiplier
+
+        self.arrange_dim_input = Rearrange(
+            "batch nchans ntimes -> batch 1 ntimes nchans"
+        )
+        # EEGNet_TC Block
+        self.eegnet_tc = _EEGNetTC(
+            n_chans=self.n_chans,
+            filter_1=self.filter_1,
+            kern_length=self.kern_length,
+            depth_multiplier=self.depth_multiplier,
+            drop_prob=self.drop_prob,
+            activation=self.activation,
+        )
+        self.arrange_dim_eegnet = Rearrange(
+            "batch filter2 rtimes 1 -> batch rtimes filter2"
+        )
+
+        # TCN Block
+        self.tcn_block = _TCNBlock(
+            input_dimension=self.filter_2,
+            depth=self.depth,
+            kernel_size=self.kernel_size,
+            filters=self.filters,
+            drop_prob=self.drop_prob,
+            activation=self.activation,
+        )
+
+        # Classification Block
+        self.final_layer = MaxNormLinear(
+            in_features=self.filters,
+            out_features=self.n_outputs,
+            max_norm_val=self.max_norm_const,
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass of the EEGTCNet model.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            Input tensor of shape (batch_size, n_chans, n_times).
+
+        Returns
+        -------
+        torch.Tensor
+            Output tensor of shape (batch_size, n_outputs).
+        """
+        # x shape: (batch_size, n_chans, n_times)
+        x = self.arrange_dim_input(x)  # (batch_size, 1, n_times, n_chans)
+        x = self.eegnet_tc(x)  # (batch_size, filter, reduced_time, 1)
+
+        x = self.arrange_dim_eegnet(x)  # (batch_size, reduced_time, F2)
+        x = self.tcn_block(x)  # (batch_size, time_steps, filters)
+
+        # Select the last time step
+        x = x[:, -1, :]  # (batch_size, filters)
+
+        x = self.final_layer(x)  # (batch_size, n_outputs)
+
+        return x
+
+
 class _EEGNetTC(nn.Module):
     """EEGNet Temporal Convolutional Network (TCN) block.
 
@@ -191,145 +333,3 @@ class _TCNBlock(nn.Module):
 
         out = out.permute(0, 2, 1)  # (batch_size, time_steps, filters)
         return out
-
-
-class EEGTCNet(EEGModuleMixin, nn.Module):
-    """EEGTCNet model from Ingolfsson et al. (2020) [ingolfsson2020]_.
-
-    .. figure:: https://braindecode.org/dev/_static/model/eegtcnet.jpg
-        :align: center
-        :alt: EEGTCNet Architecture
-
-    Combining EEGNet and TCN blocks.
-
-    Parameters
-    ----------
-    activation : nn.Module, optional
-        Activation function to use. Default is `nn.ELU()`.
-    depth_multiplier : int, optional
-        Depth multiplier for the depthwise convolution. Default is 2.
-    filter_1 : int, optional
-        Number of temporal filters in the first convolutional layer. Default is 8.
-    kern_length : int, optional
-        Length of the temporal kernel in the first convolutional layer. Default is 64.
-    dropout : float, optional
-        Dropout rate. Default is 0.5.
-    depth : int, optional
-        Number of residual blocks in the TCN. Default is 2.
-    kernel_size : int, optional
-        Size of the temporal convolutional kernel in the TCN. Default is 4.
-    filters : int, optional
-        Number of filters in the TCN convolutional layers. Default is 12.
-    max_norm_const : float
-        Maximum L2-norm constraint imposed on weights of the last
-        fully-connected layer. Defaults to 0.25.
-
-    References
-    ----------
-    .. [ingolfsson2020] Ingolfsson, T. M., Hersche, M., Wang, X., Kobayashi, N.,
-        Cavigelli, L., & Benini, L. (2020). EEG-TCNet: An accurate temporal
-        convolutional network for embedded motor-imagery brain–machine interfaces.
-        https://doi.org/10.48550/arXiv.2006.00622
-    """
-
-    def __init__(
-        self,
-        # Signal related parameters
-        n_chans=None,
-        n_outputs=None,
-        n_times=None,
-        chs_info=None,
-        input_window_seconds=None,
-        sfreq=None,
-        # Model parameters
-        activation: nn.Module = nn.ELU,
-        depth_multiplier: int = 2,
-        filter_1: int = 8,
-        kern_length: int = 64,
-        drop_prob: float = 0.5,
-        depth: int = 2,
-        kernel_size: int = 4,
-        filters: int = 12,
-        max_norm_const: float = 0.25,
-    ):
-        super().__init__(
-            n_outputs=n_outputs,
-            n_chans=n_chans,
-            chs_info=chs_info,
-            n_times=n_times,
-            input_window_seconds=input_window_seconds,
-            sfreq=sfreq,
-        )
-        del n_outputs, n_chans, chs_info, n_times, input_window_seconds, sfreq
-
-        self.activation = activation
-        self.drop_prob = drop_prob
-        self.depth_multiplier = depth_multiplier
-        self.filter_1 = filter_1
-        self.kern_length = kern_length
-        self.depth = depth
-        self.kernel_size = kernel_size
-        self.filters = filters
-        self.max_norm_const = max_norm_const
-        self.filter_2 = self.filter_1 * self.depth_multiplier
-
-        self.arrange_dim_input = Rearrange(
-            "batch nchans ntimes -> batch 1 ntimes nchans"
-        )
-        # EEGNet_TC Block
-        self.eegnet_tc = _EEGNetTC(
-            n_chans=self.n_chans,
-            filter_1=self.filter_1,
-            kern_length=self.kern_length,
-            depth_multiplier=self.depth_multiplier,
-            drop_prob=self.drop_prob,
-            activation=self.activation,
-        )
-        self.arrange_dim_eegnet = Rearrange(
-            "batch filter2 rtimes 1 -> batch rtimes filter2"
-        )
-
-        # TCN Block
-        self.tcn_block = _TCNBlock(
-            input_dimension=self.filter_2,
-            depth=self.depth,
-            kernel_size=self.kernel_size,
-            filters=self.filters,
-            drop_prob=self.drop_prob,
-            activation=self.activation,
-        )
-
-        # Classification Block
-        self.final_layer = MaxNormLinear(
-            in_features=self.filters,
-            out_features=self.n_outputs,
-            max_norm_val=self.max_norm_const,
-        )
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """
-        Forward pass of the EEGTCNet model.
-
-        Parameters
-        ----------
-        x : torch.Tensor
-            Input tensor of shape (batch_size, n_chans, n_times).
-
-        Returns
-        -------
-        torch.Tensor
-            Output tensor of shape (batch_size, n_outputs).
-        """
-        # x shape: (batch_size, n_chans, n_times)
-        x = self.arrange_dim_input(x)  # (batch_size, 1, n_times, n_chans)
-        x = self.eegnet_tc(x)  # (batch_size, filter, reduced_time, 1)
-
-        x = self.arrange_dim_eegnet(x)  # (batch_size, reduced_time, F2)
-        x = self.tcn_block(x)  # (batch_size, time_steps, filters)
-
-        # Select the last time step
-        x = x[:, -1, :]  # (batch_size, filters)
-
-        x = self.final_layer(x)  # (batch_size, n_outputs)
-
-        return x
