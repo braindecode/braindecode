e8ff0ff29579ec2d5614d21a366fba83ea945844
bruAristimunha <b.aristimunha@gmail.com>
2025-08-01 17:03:25 +0200

    updating small patching to solve the issue

--- test/unit_tests/models/test_models.py
+++ test/unit_tests/models/test_models.py
@@ -1483,3 +1483,20 @@ def test_batchnorm_deep4net(batch_norm):
     model = Deep4Net(n_outputs=2, n_chans=22, n_times=1000, batch_norm=batch_norm)
     
     assert model is not None
+
+def test_fc_length_eegconformer():
+    """
+    Test the number of trainable parameters in EEGConformer model.
+    """
+    model = EEGConformer(
+        n_chans=64,  # Number of EEG channels
+        n_outputs=2,  # Number of output classes
+        n_times=500,  # Length of the input sequence (e.g., 500 time steps)
+        final_fc_length=120,
+        input_window_seconds=1.0,
+        return_features=True,
+        drop_prob=0.5,  # Dropout probability
+        sfreq=500.0  # Sampling frequency of the EEG data
+    )
+
+    assert model is not None
