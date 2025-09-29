a0f5d95148d1e61c5695115db31bd18d5c7f2e23
Daniel Wu <wurining@gmail.com>
2025-09-29 17:01:35 +0100

    fix(preprocess): set prefer option to "threads". Backend---Loky requires all task can be pickled.

--- braindecode/preprocessing/preprocess.py
+++ braindecode/preprocessing/preprocess.py
@@ -153,8 +153,7 @@ def preprocess(
 
     parallel_processing = (n_jobs is not None) and (n_jobs != 1)
 
-    job_prefer = "threads" if platform.system() == "Windows" else None
-    list_of_ds = Parallel(n_jobs=n_jobs, prefer=job_prefer)(
+    list_of_ds = Parallel(n_jobs=n_jobs, prefer="threads")(
         delayed(_preprocess)(
             ds,
             i + offset,
