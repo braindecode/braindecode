23a014cb30326411cf0e5bc2684a75c663e6635b
Bru <a.bruno@aluno.ufabc.edu.br>
2025-11-05 19:17:18 +0100

    Apply suggestion from @bruAristimunha

--- braindecode/models/util.py
+++ braindecode/models/util.py
@@ -82,7 +82,7 @@ models_mandatory_parameters = [
     ("EEGSym", ["chs_info", "n_chans", "n_outputs", "n_times", "sfreq"], None),
     ("TSception", ["n_chans", "n_outputs", "n_times", "sfreq"], dict(sfreq=200.0)),
     ("EEGTCNet", ["n_chans", "n_outputs", "n_times"], None),
-    ("SyncNet", ["n_chans", "n_outputs", "n_times", "sfreq"], None),
+    ("SyncNet", ["n_chans", "n_outputs", "n_times"], None),
     ("MSVTNet", ["n_chans", "n_outputs", "n_times"], None),
     ("EEGMiner", ["n_chans", "n_outputs", "n_times", "sfreq"], dict(sfreq=200.0)),
     ("CTNet", ["n_chans", "n_outputs", "n_times"], None),
