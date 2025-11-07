d543f6a3b1e1d425b7113d1724870bcf7758db46
Pierre Guetschel <pierre.guetschel@donders.ru.nl>
2025-11-07 15:06:43 +0100

    Better mne preprocessings and repr

--- braindecode/preprocessing/mne_preprocess.py
+++ braindecode/preprocessing/mne_preprocess.py
@@ -16,18 +16,50 @@ def _generate_init_method(func):
     """
     Generate an __init__ method for a class based on the function's signature.
     """
+    func_name = func.__name__
     parameters = list(inspect.signature(func).parameters.values())
-    param_names = [param.name for param in parameters]
+    param_names = [param.name for param in parameters if param.name != "self"]
+    all_mandatory = [
+        param.name
+        for param in parameters
+        if param.default == inspect.Parameter.empty and param.name != "self"
+    ]
 
     def init_method(self, *args, **kwargs):
+        used = []
+        mandatory = list(all_mandatory)
+        init_kwargs = {}
         for name, value in zip(param_names, args):
-            setattr(self, name, value)
+            init_kwargs[name] = value
+            used.append(name)
+            if name in mandatory:
+                mandatory.remove(name)
         for name, value in kwargs.items():
-            setattr(self, name, value)
-        self.kwargs = kwargs
+            if name in used:
+                raise TypeError(f"Multiple values for argument '{name}'")
+            if name not in param_names:
+                raise TypeError(
+                    f"'{name}' is an invalid keyword argument for {func_name}()"
+                )
+            init_kwargs[name] = value
+            if name in mandatory:
+                mandatory.remove(name)
+        if len(mandatory) > 0:
+            raise TypeError(
+                f"{func_name}() missing required arguments: {', '.join(mandatory)}"
+            )
+        Preprocessor.__init__(self, fn=func_name, apply_on_array=False, **init_kwargs)
 
     init_method.__signature__ = inspect.signature(func)
     return init_method
+
+
+def _generate_repr_method(class_name):
+    def repr_method(self):
+        args_str = ", ".join(f"{k}={v.__repr__()}" for k, v in self.kwargs.items())
+        return f"{class_name}({args_str})"
+
+    return repr_method
 
 
 def _generate_mne_pre_processor(function):
@@ -44,8 +76,7 @@ def _generate_mne_pre_processor(function):
     class_attrs = {
         "__init__": _generate_init_method(function),
         "__doc__": _update_moabb_docstring(function, doc),
-        "fn": function.__name__,
-        "apply_on_array": False,
+        "__repr__": _generate_repr_method(class_name),
     }
     generated_class = type(class_name, base_classes, class_attrs)
 
