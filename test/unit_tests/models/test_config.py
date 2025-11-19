9d0d7af77cf2338365b46078af677b81f05d445b
Pierre Guetschel <pierre.guetschel@donders.ru.nl>
2025-11-20 00:06:30 +0100

    Improve test coverage by testing all allowed combinations of signal parameters for each model config

--- test/unit_tests/models/test_config.py
+++ test/unit_tests/models/test_config.py
@@ -6,8 +6,12 @@ if pydantic is None:
     pytest.skip("pydantic not installed, skipping", allow_module_level=True)
 
 from braindecode.models.config import make_model_config
-from braindecode.models.util import _get_signal_params as get_sp
-from braindecode.models.util import models_dict, models_mandatory_parameters
+from braindecode.models.util import (
+    _get_possible_signal_params,
+    _get_signal_params,
+    models_dict,
+    models_mandatory_parameters,
+)
 
 
 @pytest.mark.parametrize(
@@ -18,7 +22,9 @@ def test_make_model_config(model_name, required, signal_params):
     model_class = models_dict[model_name]
     ModelConfig = make_model_config(model_class, required)
 
-    sp = get_sp(signal_params, required)
-    cfg = ModelConfig(**sp)
-    model = cfg.create_instance()
-    assert isinstance(model, model_class)
+    sp = _get_signal_params(signal_params)
+    model_kwargs_list = _get_possible_signal_params(sp, required)
+    for model_kwargs in model_kwargs_list:
+        cfg = ModelConfig(**model_kwargs)
+        model = cfg.create_instance()
+        assert isinstance(model, model_class)
