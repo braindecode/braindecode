4e50be093db3c8f64ceebfa2d43ca1dfbd9e96c9
Fashad Ahmed <fashad.ahmed20@gmail.com>
2026-07-26 23:59:40 +0200

    feat(models): include SleepFM and SleepFMStager in model exports
    Add SleepFM and SleepFMStager to the braindecode.models module, updating the __all__ export to ensure they are accessible for users. This change enhances the model offerings for multimodal polysomnography analysis.

--- braindecode/models/__init__.py
+++ braindecode/models/__init__.py
@@ -54,6 +54,7 @@ from .signal_jepa import (
 from .sinc_shallow import SincShallowNet
 from .sleep_stager_blanco_2020 import SleepStagerBlanco2020
 from .sleep_stager_chambon_2018 import SleepStagerChambon2018
+from .sleepfm import SleepFM, SleepFMStager
 from .sparcnet import SPARCNet
 from .sstdpn import SSTDPN
 from .steegformer import STEEGFormer
@@ -134,6 +135,8 @@ __all__ = [
     "SignalJEPA_PreLocal",
     "SincShallowNet",
     "SSTDPN",
+    "SleepFM",
+    "SleepFMStager",
     "SleepStagerBlanco2020",
     "SleepStagerChambon2018",
     "SPARCNet",
